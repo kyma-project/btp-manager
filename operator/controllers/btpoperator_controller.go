f0f75ccc7a79541fd5326aa4206b6fbedba768a6
Marcin Szwed <marcin.szwed@sap.com>
2022-10-05 12:29:32 +0200

    Restore default kubebuilder controller

--- operator/controllers/btpoperator_controller.go
+++ operator/controllers/btpoperator_controller.go
@@ -17,78 +17,45 @@ limitations under the License.
 package controllers
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/kyma-project/btp-manager/operator/api/v1alpha1"
-	"github.com/kyma-project/module-manager/operator/pkg/declarative"
-	"github.com/kyma-project/module-manager/operator/pkg/types"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-)
-
-const (
-	operatorName = "btp-manager"
-	labelKey     = "app.kubernetes.io/managed-by"
-	chartPath    = "./module-chart"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // BtpOperatorReconciler reconciles a BtpOperator object
 type BtpOperatorReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
-	declarative.ManifestReconciler
-	*rest.Config
 }
 
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=btpoperators,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=btpoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=btpoperators/finalizers,verbs=update
-//+kubebuilder:rbac:groups="",resources=events,verbs=create;patch;get;list;watch
-//+kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the BtpOperator object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
+func (r *BtpOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *BtpOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	r.Config = mgr.GetConfig()
-	if err := r.initReconciler(mgr); err != nil {
-		return err
-	}
-
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.BtpOperator{}).
 		Complete(r)
-}
-
-func (r *BtpOperatorReconciler) initReconciler(mgr ctrl.Manager) error {
-	manifestResolver := &ManifestResolver{}
-	return r.Inject(mgr, &v1alpha1.BtpOperator{},
-		declarative.WithManifestResolver(manifestResolver),
-		declarative.WithCustomResourceLabels(map[string]string{labelKey: operatorName}),
-		// declarative.WithPostRenderTransform(transform),
-		declarative.WithResourcesReady(true),
-	)
-}
-
-/*
-// transform modifies the resources based on some criteria, before installation.
-func transform(_ context.Context, _ types.BaseCustomObject, manifestResources *types.ManifestResources) error {
-
-}
-*/
-
-// ManifestResolver represents the chart information for the passed Sample resource.
-type ManifestResolver struct{}
-
-// Get returns the chart information to be processed.
-func (m *ManifestResolver) Get(obj types.BaseCustomObject) (types.InstallationSpec, error) {
-	btpServiceOperator, valid := obj.(*v1alpha1.BtpOperator)
-	if !valid {
-		return types.InstallationSpec{},
-			fmt.Errorf("invalid type conversion for %s", client.ObjectKeyFromObject(obj))
-	}
-	return types.InstallationSpec{
-		ChartPath:   chartPath,
-		ReleaseName: btpServiceOperator.Spec.ReleaseName,
-	}, nil
 }
