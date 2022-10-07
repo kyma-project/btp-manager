a7c14a3a56e869df41f3436871b185f23b36110f
LJ <lukasz.jezak@sap.com>
2022-10-07 09:44:39 +0200

    Deprovisioning WiP

--- operator/controllers/btpoperator_controller.go
+++ operator/controllers/btpoperator_controller.go
@@ -15,12 +15,23 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/kyma-project/btp-manager/operator/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+const (
+	btpOperatorGroup           = "services.cloud.sap.com"
+	btpOperatorApiVer          = "v1"
+	btpOperatorServiceInstance = "ServiceInstance"
+	btpOperatorServiceBinding  = "ServiceBinding"
 )
 
 // BtpOperatorReconciler reconciles a BtpOperator object
@@ -45,7 +56,7 @@ type BtpOperatorReconciler struct {
 func (r *BtpOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	_, _ = r.HandleDeprovisioning(ctx, req)
 
 	return ctrl.Result{}, nil
 }
@@ -55,4 +66,70 @@ func (r *BtpOperatorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.BtpOperator{}).
 		Complete(r)
+}
+
+func (r *BtpOperatorReconciler) HandleDeprovisioning(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+
+	btpManager := v1alpha1.BtpOperator{}
+	if err := r.Get(ctx, req.NamespacedName, &btpManager); err != nil {
+		logger.Error(err, "failed to get btp manager")
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, err
+	}
+
+	markedForDelete := !btpManager.ObjectMeta.DeletionTimestamp.IsZero()
+	if markedForDelete {
+		logger.Info("btp operator is under deletion")
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *BtpOperatorReconciler) GetBtpGvk(kind string) (schema.GroupVersionKind, error) {
+	if kind != btpOperatorServiceBinding && kind != btpOperatorServiceInstance {
+		return schema.GroupVersionKind{}, fmt.Errorf("%s as kind not supported", kind)
+	}
+	return schema.GroupVersionKind{Version: btpOperatorApiVer, Group: btpOperatorGroup, Kind: kind}, nil
+}
+
+func (r *BtpOperatorReconciler) HardDelete(ctx *context.Context, gvk schema.GroupVersionKind) error {
+	if ctx == nil {
+		return fmt.Errorf("hard delete: ctx not set")
+	}
+
+	obj := unstructured.Unstructured{}
+	obj.SetGroupVersionKind(gvk)
+	if err := r.DeleteAllOf(*ctx, &obj); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *BtpOperatorReconciler) SoftDelete(ctx *context.Context, gvk schema.GroupVersionKind) []error {
+	errs := make([]error, 0)
+
+	list := &unstructured.UnstructuredList{}
+	list.SetGroupVersionKind(gvk)
+	if err := r.List(*ctx, list); err != nil {
+		errs = append(errs, err)
+		return errs
+	}
+
+	for _, item := range list.Items {
+		item.SetFinalizers([]string{})
+		if err := r.Update(*ctx, &item); err != nil {
+			errs = append(errs, fmt.Errorf("error occurred in soft delete when updating btp operator resources"))
+		}
+	}
+
+	return nil
+}
+
+func (r *BtpOperatorReconciler) RemoveInstalledResources() {
+
 }
