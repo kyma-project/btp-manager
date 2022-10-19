1511d9ef2726e78ad7a95ff32cefb0e1daaeb531
LJ <lukasz.jezak@sap.com>
2022-10-19 15:33:53 +0200

    Deprovisioning

--- operator/controllers/btpoperator_controller_test.go
+++ operator/controllers/btpoperator_controller_test.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/kyma-project/btp-manager/operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -12,16 +15,32 @@ import (
 	"time"
 )
 
-var _ = Describe("deprovisioning - safe delete test", func() {
+/*var _ = Describe("deprovisioning - safe delete test", func() {
 	It("soft delete should succeed", func() {
 		triggerDelete(NewCfg(time.Nanosecond))
 		doChecks()
 	})
 
-})
+})*/
 
 var _ = Describe("deprovisioning - hard delete test", func() {
 	It("hard delete should succeed", func() {
+
+		bindingGvk := schema.GroupVersionKind{Version: btpOperatorApiVer, Group: btpOperatorGroup, Kind: btpOperatorServiceBinding}
+		instanceGvk := schema.GroupVersionKind{Version: btpOperatorApiVer, Group: btpOperatorGroup, Kind: btpOperatorServiceInstance}
+
+		//
+		var err error
+		err = DeleteDeployment()
+		Expect(err).To(BeNil())
+		err = DeleteWebhooks()
+		Expect(err).To(BeNil())
+		err = SoftDelete(bindingGvk)
+		Expect(err).To(BeNil())
+		err = SoftDelete(instanceGvk)
+		Expect(err).To(BeNil())
+		//
+
 		triggerDelete(nil)
 		doChecks()
 	})
@@ -95,4 +114,57 @@ func checkIfNoBtpResourceExists() {
 	}
 
 	Expect(found).To(BeFalse())
+}
+
+func DeleteDeployment() error {
+	deployment := &appsv1.Deployment{}
+	deployment.Name = "sap-btp-operator-controller-manager"
+	deployment.Namespace = "default"
+	if err := k8sClient.Delete(ctx, deployment); err != nil {
+		return err
+	}
+	return nil
+}
+
+func SoftDelete(gvk schema.GroupVersionKind) error {
+	errs := fmt.Errorf("")
+	listGvk := gvk
+	listGvk.Kind = gvk.Kind + "List"
+	list := &unstructured.UnstructuredList{}
+	list.SetGroupVersionKind(listGvk)
+	if errs := k8sClient.List(ctx, list); errs != nil {
+		errs = fmt.Errorf("%w; could not list in soft delete", errs)
+		return errs
+	}
+
+	for _, item := range list.Items {
+		item.SetFinalizers([]string{})
+		if err := k8sClient.Update(ctx, &item); err != nil {
+			errs = fmt.Errorf("%w; error occurred in soft delete when updating btp operator resources", err)
+		}
+	}
+
+	if errs != nil && len(errs.Error()) > 0 {
+		return errs
+	} else {
+		return nil
+	}
+}
+
+func DeleteWebhooks() error {
+	mutatingWebHook := &admissionregistrationv1.MutatingWebhookConfiguration{}
+	mutatingWebHook.Name = "sap-btp-operator-mutating-webhook-configuration"
+
+	if err := k8sClient.Delete(ctx, mutatingWebHook); err != nil {
+		return err
+	}
+
+	validatingWebhook := &admissionregistrationv1.ValidatingWebhookConfiguration{}
+	validatingWebhook.Name = "sap-btp-operator-validating-webhook-configuration"
+
+	if err := k8sClient.Delete(ctx, validatingWebhook); err != nil {
+		return err
+	}
+
+	return nil
 }
