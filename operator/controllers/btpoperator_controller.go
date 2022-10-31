6b8288ba71624eadbe016e7f988ffbe3a42ceb8f
Marcin Szwed <marcin.szwed@sap.com>
2022-10-31 10:21:40 +0100

    Enable adding finalizers

--- operator/controllers/btpoperator_controller.go
+++ operator/controllers/btpoperator_controller.go
@@ -35,6 +35,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -65,15 +66,6 @@ type BtpOperatorReconciler struct {
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=btpoperators/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the BtpOperator object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *BtpOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
@@ -93,12 +85,9 @@ func (r *BtpOperatorReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		}
 	*/
 
-	// temporary disable adding finalizer
-	/*
-		if ctrlutil.AddFinalizer(cr, deletionFinalizer) {
-			return ctrl.Result{}, r.Update(ctx, cr)
-		}
-	*/
+	if ctrlutil.AddFinalizer(cr, deletionFinalizer) {
+		return ctrl.Result{}, r.Update(ctx, cr)
+	}
 
 	switch cr.Status.State {
 	case "":
