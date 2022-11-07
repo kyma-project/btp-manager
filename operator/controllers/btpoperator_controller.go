fb45a8d731bd1fdd776e8e328e7253a1ccd8a0e7
LJ <lukasz.jezak@sap.com>
2022-11-07 13:46:19 +0100

    Fix imports

--- operator/controllers/btpoperator_controller.go
+++ operator/controllers/btpoperator_controller.go
@@ -42,7 +42,6 @@ import (
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
-
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
