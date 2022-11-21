b90533cf43f3d2eb1ce55360d09ad28e2134c152
Till Knuesting <t.knuesting@sap.com>
2022-11-21 15:41:35 +0000

    CI-CD

--- testing/k3s-setup.sh
+++ testing/k3s-setup.sh
@@ -8,10 +8,10 @@ docker run -d \
 -p 5000:5000 \
 --restart=always \
 --name registry.localhost \
--v $PWD/registry:/var/lib/registry \
+-v "$PWD/registry:/var/lib/registry" \
 registry:2
 
-echo "Starting K3s v1.24.7+k3s1 cluster"
+echo "Starting K3s cluster"
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="v1.24.7+k3s1" K3S_KUBECONFIG_MODE=777 INSTALL_K3S_EXEC="server --disable traefik" sh -
 mkdir -p ~/.kube
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
