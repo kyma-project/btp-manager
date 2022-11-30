4121fdb953f3fce99eaf76a7159428c969838036
Piotr Kopczyński <piotr.kopczynski@sap.com>
2022-11-30 14:41:39 +0100

    Util to generate sap-btp-manager secret from input json file (#43)

---
+++ hack/create-secret-file.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+FILE=creds.json
+if [ ! -f "$FILE" ];
+then
+    echo "Required file: $FILE does not exist."
+    exit
+fi
+
+cat <<EOF > operator-secret.yaml
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: sap-btp-manager
+  namespace: kyma-system
+data:
+  clientid: $(jq --raw-output '.clientid | @base64' creds.json)
+  clientsecret: $(jq --raw-output '.clientsecret | @base64' creds.json)
+  sm_url: $(jq --raw-output '.sm_url | @base64' creds.json)
+  tokenurl: $(jq --raw-output '.url | @base64' creds.json)
+  cluster_id: dGVzdF9jbHVzdGVyX2lk
+EOF
+
+echo "operator-secret.yaml file created."
