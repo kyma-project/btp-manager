807ebd9432ddf9bc29cf75bb6813ed5c4bb88513
LJ <lukasz.jezak@sap.com>
2022-12-06 15:22:20 +0100

    Update & Version

--- operator/internal/renamer.go
+++ operator/internal/renamer.go
@@ -1,1 +1,42 @@
 package ymlutils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func TransformCharts(chartPath string, suffix string) error {
+	root := fmt.Sprintf("%s/templates/", chartPath)
+	if err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !strings.HasSuffix(info.Name(), ".yml") {
+			return nil
+		}
+
+		filename := fmt.Sprintf("%s/%s", root, info.Name())
+		input, err := os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+
+		lines := strings.Split(string(input), "\n")
+		for i, line := range lines {
+			line = strings.TrimSpace(line)
+			if strings.HasPrefix(line, "name:") {
+				lines[i] = lines[i] + suffix
+			}
+		}
+		output := strings.Join(lines, "\n")
+		err = os.WriteFile(filename, []byte(output), 0644)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
--- operator/main.go
+++ operator/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
 	"time"
@@ -25,6 +26,7 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -43,7 +45,8 @@ var (
 )
 
 const (
-	timeout = time.Minute * 20
+	timeout   = time.Minute * 20
+	chartPath = "./module-chart"
 )
 
 func init() {
@@ -105,6 +108,16 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "BtpOperator")
 		os.Exit(1)
 	}
+
+	err = mgr.Add(manager.RunnableFunc(func(ctx context.Context) error {
+		return reconciler.StoreChartDetails(ctx, chartPath)
+	}))
+
+	if err != nil {
+		setupLog.Error(err, "unable add a runnable to the manager, which sets versioning")
+		os.Exit(1)
+	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
