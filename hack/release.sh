e9ba0c6223905f47cee257303a6a9b87e4ee0bcb
Piotr Miskiewicz <piotr.miskiewicz@sap.com>
2023-01-12 15:14:50 +0100

    wip

--- hack/release.sh
+++ hack/release.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # standard bash error handling
-set -o nounset # treat unset variables as an error and exit immediately.
-set -o errexit # exit immediately when a command fails.
-set -E         # needs to be set if we want the ERR trap
-set -o pipefail
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
 
 
 get_new_release_version() {
