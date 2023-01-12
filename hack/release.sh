7f02bc1262a2df80d686b694b10594de6fa9f1ae
Piotr Miskiewicz <piotr.miskiewicz@sap.com>
2023-01-12 15:14:04 +0100

    pipefail

--- hack/release.sh
+++ hack/release.sh
@@ -4,6 +4,7 @@
 set -o nounset # treat unset variables as an error and exit immediately.
 set -o errexit # exit immediately when a command fails.
 set -E         # needs to be set if we want the ERR trap
+set -o pipefail
 
 
 get_new_release_version() {
