22103f9f66499f120243026e49c0bdc11551c82b
Piotr Miskiewicz <piotr.miskiewicz@sap.com>
2023-01-23 14:49:19 +0100

    Release script

--- hack/release.sh
+++ hack/release.sh
@@ -6,30 +6,45 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
+# Expected variables:
+#   PULL_BASE_REF - name of the tag
+#   BOT_GITHUB_TOKEN - github token used to upload the template yaml
 
-get_new_release_version() {
-    # get the list of tags in a reverse chronological order
-    TAG_LIST=($(git tag --sort=-creatordate))
-    NEW_RELEASE_VERSION=${TAG_LIST[0]}
-}
+echo "PULL_BASE_REF ${PULL_BASE_REF}"
 
-get_current_release_version() {
-    # get the list of tags in a reverse chronological order excluding release candidate tags
-    TAG_LIST_WITHOUT_RC=($(git tag --sort=-creatordate | grep -v -e "-rc"))
-    if [[ $NEW_RELEASE_VERSION == *"-rc"* ]]; then
-        CURRENT_RELEASE_VERSION=${TAG_LIST_WITHOUT_RC[0]}
-    else
-        CURRENT_RELEASE_VERSION=${TAG_LIST_WITHOUT_RC[1]}
-    fi
-}
+MODULE_VERSION=${PULL_BASE_REF} make module-build
 
+echo "Generated template.yaml:"
+cat template.yaml
 
-if [[ "${1-}" == "ci" ]]; then
-      git remote add origin git@github.com:kyma-project/cli.git
+echo "\nUpdating github release with template.yaml"
+
+echo "\nFinding release: ${PULL_BASE_REF}"
+releases=$(curl -sL \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $BOT_GITHUB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/kyma-project/btp-manager/releases/tags/${PULL_BASE_REF})
+
+release_id=$(echo $releases | jq -r '.id')
+echo "Got release ID: ${release_id}"
+
+GH_ASSET="https://uploads.github.com/repos/kyma-project/btp-manager/releases/${release_id}/assets?name=$(basename $filename)"
+
+echo "Release asset url: ${GH_ASSET}"
+
+response=$(curl -s -o output.txt -w "%{http_code}" \
+                --request POST --data-binary @"$filename" \
+                -H "Authorization: token $BOT_GITHUB_TOKEN" \
+                -H "Content-Type: text/yaml" \
+                 $GH_ASSET)
+
+if [[ "$response" != "201" ]]; then
+  echo "Unable to upload the asset: "
+  echo "HTTP Status: $response"
+  cat output.txt
+  exit 1
+else
+  echo "template.yaml uploaded"
 fi
 
-get_new_release_version
-
-echo "Preparing release ${NEW_RELEASE_VERSION}"
-
-MODULE_VERSION=${NEW_RELEASE_VERSION} make docker-build docker-push module-build
