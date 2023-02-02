70ff091f75c0251045c6fa17a9390a52db614143
LJ <lukasz.jezak@sap.com>
2023-02-02 12:32:47 +0100

    improvments

--- hack/update/get-latest-chart-version.sh
+++ hack/update/get-latest-chart-version.sh
@@ -7,4 +7,4 @@ latest=$(curl \
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         https://api.github.com/repos/SAP/sap-btp-service-operator/releases/latest | jq -r '.tag_name')
-echo "v0.2.5"
+echo $latest
