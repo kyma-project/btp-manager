1e3e86e959bdfea9920321bb3d5163fb7b39a424
jaroslaw-pieszka <jaroslaw.pieszka@sap.com>
2023-02-21 16:11:13 +0100

    moving scripts around

--- testing/k3s-setup.sh
+++ testing/k3s-setup.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-set -o errexit
+#!/usr/bin/env bash
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
 
 echo "Starting docker registry"
 sudo mkdir -p /etc/rancher/k3s
