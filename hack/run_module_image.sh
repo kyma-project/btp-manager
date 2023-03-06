a57e0cc573229db54aff279ff86b9ab22cb301fd
Piotr Kopczyński <piotr.kopczynski@sap.com>
2023-03-06 15:16:49 +0100

    Improvements in the BTP Manager installation area (#210)
    * Improvements in the BTP Manager installation area
    
    * Improvements in the BTP Manager installation area - review fixes
    
    * Apply suggestions from code review
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>
    
    ---------
    
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>

--- hack/run_module_image.sh
+++ hack/run_module_image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script has the following argument: a link to a module image, for example:
-# ./run_module_image.sh europe-docker.pkg.dev/kyma-project/prod/unsigned/component-descriptors/kyma.project.io/module/btp-operator:0.0.32
+# ./run_module_image.sh europe-docker.pkg.dev/kyma-project/prod/unsigned/component-descriptors/kyma.project.io/module/btp-operator:v0.2.3
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -9,19 +9,18 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
-echo "Downloading module image"
-
+echo -e "\n--- Downloading module image"
+rm -r downloaded_module
 mkdir downloaded_module
-skopeo copy docker://$1 dir:downloaded_module
 
-cd downloaded_module
-filename=$(cat manifest.json  | jq -c '.layers[] | select(.mediaType=="application/gzip").digest[7:]' | tr -d \")
+downloaded_dir=downloaded_module
+skopeo copy docker://$1 dir:$downloaded_dir
+filename=$(cat $downloaded_dir/manifest.json  | jq -c '.layers[] | select(.mediaType=="application/gzip").digest[7:]' | tr -d \")
 
-echo "Extracting $filename"
+echo -e "\n--- Extracting resources from file:" $filename
+mkdir $downloaded_dir/chart
+tar -xzf $downloaded_dir/$filename -C $downloaded_dir/chart
 
-mkdir chart
-tar -xzf $filename -C chart
-
-echo "Installing helm chart"
-
-helm install btp-manager chart
+echo -e "\n--- Installing BTP Manager"
+# install by helm
+helm upgrade --install btp-manager $downloaded_dir/chart -n kyma-system --create-namespace
