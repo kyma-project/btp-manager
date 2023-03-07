93042b34fa9555e38b5ca12f0f46299844dd9329
LJ <lukasz.jezak@sap.com>
2023-03-07 16:10:56 +0100

    wip - temp commit msg

--- testing/set-env-vars.sh
+++ testing/set-env-vars.sh
@@ -15,7 +15,7 @@ export USE_EXISTING_CLUSTER=${USE_EXISTING_CLUSTER:="false"}
 export SINGLE_TEST_TIMEOUT=${SINGLE_TEST_TIMEOUT:="10s"}
 
 # if you plan to debug or run on existing cluster increase the timeout (180 seconds should be ok)
-export SUITE_TIMEOUT=${SUITE_TIMEOUT:=45s}
+export SUITE_TIMEOUT=${SUITE_TIMEOUT:=60s}
 
 # setting verbose output for ginkgo
 # for very verbose set "ginkgo.vv"
