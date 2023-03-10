258c0dce986fd05c315761ccfb8866b638183b96
LJ <lukasz.jezak@sap.com>
2023-03-10 12:27:53 +0100

    Cleanup

--- afterrunmain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f deployments/prerequisites.yaml &&
-chmod +x hack/create-secret-file.sh &&
-hack/create-secret-file.sh &&
-kubectl apply -f hack/operator-secret.yaml &&
-kubectl apply -f examples/btp-operator.yaml
--- main.go
+++ main.go
@@ -19,8 +19,6 @@ package main
 import (
 	"flag"
 	"os"
-	"os/exec"
-
 	//test
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
@@ -53,10 +51,6 @@ func init() {
 }
 
 func main() {
-	cmd := exec.Command("/bin/sh", "preparemain.sh")
-	if err := cmd.Run(); err != nil {
-		panic(err)
-	}
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
