27492b329f5639281f81ffffb443dc8a47f8ca26
Marcin Szwed <marcin.szwed@sap.com>
2023-03-16 13:58:06 +0100

    Disable btp-manager Metrics and Health servers to allow tests parallelization

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -178,7 +178,9 @@ var _ = BeforeSuite(func() {
 	Expect(k8sClient).NotTo(BeNil())
 
 	k8sManager, err = ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme.Scheme,
+		Scheme:                 scheme.Scheme,
+		MetricsBindAddress:     "0",
+		HealthProbeBindAddress: "0",
 	})
 	Expect(err).ToNot(HaveOccurred())
 
