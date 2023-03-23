eca9c7dd61ee3a6ea8ff6d92c6e449ad8d53baf8
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-03-23 13:04:54 +0100

    Update hack/release.sh
    Co-authored-by: Jarosław Pieszka <jaroslaw.pieszka@sap.com>

--- hack/release.sh
+++ hack/release.sh
@@ -38,7 +38,6 @@ cat template.yaml
 
 sed 's/target: remote/target: control-plane/g' <template.yaml >template_control_plane.yaml
 
-sed -i '' 's/target: remote/target: control-plane/g' template_control_plane.yaml
 echo "Generated template_control_plane.yaml:"
 cat template_control_plane.yaml
 
