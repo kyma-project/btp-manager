b5295901383ba13c8da4d282487a2bc0ce10e708
jaroslaw-pieszka <jaroslaw.pieszka@sap.com>
2023-03-25 15:04:22 +0100

    singular instead of plural

--- hack/release.sh
+++ hack/release.sh
@@ -44,13 +44,13 @@ cat template_control_plane.yaml
 echo "Updating github release with template.yaml, template_control_plane.yaml, rendered.yaml"
 
 echo "Finding release: ${PULL_BASE_REF}"
-releases=$(curl -sL \
+release=$(curl -sL \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $BOT_GITHUB_TOKEN"\
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/kyma-project/btp-manager/releases/tags/${PULL_BASE_REF})
 
-release_id=$(echo $releases | jq -r '.id')
+release_id=$(echo $release | jq -r '.id')
 echo "Got release ID: ${release_id}"
 
 TEMPLATE_GH_ASSET="https://uploads.github.com/repos/kyma-project/btp-manager/releases/${release_id}/assets?name=template.yaml"
