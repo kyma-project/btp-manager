6865766439616f69b851319721fc3b3594c868aa
Marcin Szwed <marcin.szwed@sap.com>
2023-03-28 08:52:00 +0200

    Move btpoperator controller tests to separate files

---
+++ controllers/btpoperator_controller_configuration_test.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("BTP Operator controller - configuration", func() {
+	Context("When the ConfigMap is present", func() {
+		It("should adjust configuration settings in the operator accordingly", func() {
+			GinkgoWriter.Println("--- PROCESS:", GinkgoParallelProcess(), "---")
+			cm := initConfig(map[string]string{"ProcessingStateRequeueInterval": "10s"})
+			reconciler.reconcileConfig(cm)
+			Expect(ProcessingStateRequeueInterval).To(Equal(time.Second * 10))
+		})
+	})
+})
