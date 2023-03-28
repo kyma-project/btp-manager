01057b88024c16c86b7131ad134f2a65e6ddfa02
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2023-03-28 08:45:58 +0200

    Update scripts/await_artifacts.sh
    Co-authored-by: Iwona Langer <iwona.langer@sap.com>

--- scripts/await_artifacts.sh
+++ scripts/await_artifacts.sh
@@ -5,7 +5,7 @@ SKIP_TEMPLATES=$3
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
-set -E          # needs to be set if we want the ERR trap
+set -E          # must be set if you want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
 # This script has the following arguments:
