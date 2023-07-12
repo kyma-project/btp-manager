075d7f148f5a8a1dc2184ed63ad6d636ed4669ff
Marcin Szwed <marcin.szwed@sap.com>
2023-07-12 13:53:23 +0200

    Refactor test for multiple BtpOperator CRs

--- controllers/btpoperator_controller_cr_rotation_test.go
+++ controllers/btpoperator_controller_cr_rotation_test.go
@@ -2,12 +2,12 @@ package controllers
 
 import (
 	"context"
-	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	"github.com/kyma-project/btp-manager/api/v1alpha1"
 	"github.com/kyma-project/btp-manager/internal/conditions"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -18,62 +18,59 @@ const (
 )
 
 var _ = Describe("BTP Operator CR leader replacement", Label("debug"), func() {
-	It("2 CRs(1-Ready,2-Error) & SI/SB exists, first is under deletion, and finalizer is removed ", func() {
-		var err error
-		ctx := context.Background()
+	Describe("Two CRs (1st-Ready, 2nd-Error) and SI/SB exist", func() {
+		When("First CR is under deletion, and finalizer is removed", func() {
+			It("should reconcile second CR as primary", func() {
+				var err error
+				ctx := context.Background()
 
-		secret, err := createCorrectSecretFromYaml()
-		Expect(k8sClient.Create(ctx, secret)).To(Succeed())
+				secret, err := createCorrectSecretFromYaml()
+				Expect(k8sClient.Create(ctx, secret)).To(Succeed())
 
-		btpOperator1 := createBtpOperator(firstBtpOperator)
-		Eventually(func() error { return k8sClient.Create(ctx, btpOperator1) }).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(Succeed())
-		Eventually(updateCh).Should(Receive(matchState(v1alpha1.StateReady)))
+				btpOperator1 := createBtpOperator(firstBtpOperator)
+				Eventually(func() error { return k8sClient.Create(ctx, btpOperator1) }).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(Succeed())
+				Eventually(updateCh).Should(Receive(matchState(v1alpha1.StateReady)))
 
-		_ = createResource(instanceGvk, kymaNamespace, instanceName)
-		ensureResourceExists(instanceGvk)
+				_ = createResource(instanceGvk, kymaNamespace, instanceName)
+				ensureResourceExists(instanceGvk)
 
-		_ = createResource(bindingGvk, kymaNamespace, bindingName)
-		ensureResourceExists(bindingGvk)
+				_ = createResource(bindingGvk, kymaNamespace, bindingName)
+				ensureResourceExists(bindingGvk)
 
-		btpOperator2 := createBtpOperator(secondBtpOperator)
-		Eventually(func() error { return k8sClient.Create(ctx, btpOperator2) }).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(Succeed())
-		Eventually(updateCh).Should(Receive(matchState(v1alpha1.StateError)))
+				btpOperator2 := createBtpOperator(secondBtpOperator)
+				Eventually(func() error { return k8sClient.Create(ctx, btpOperator2) }).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(Succeed())
+				Eventually(updateCh).Should(Receive(matchState(v1alpha1.StateError)))
 
-		btpOperators := &v1alpha1.BtpOperatorList{}
-		err = k8sClient.List(ctx, btpOperators)
-		Expect(err).To(BeNil())
-		Expect(len(btpOperators.Items)).To(BeEquivalentTo(2))
+				btpOperators := &v1alpha1.BtpOperatorList{}
+				err = k8sClient.List(ctx, btpOperators)
+				Expect(err).To(BeNil())
+				Expect(len(btpOperators.Items)).To(BeEquivalentTo(2))
 
-		btpOperatorInErrorExists := false
-		btpOperatorInReadyExists := false
-		for _, btpOperator := range btpOperators.Items {
-			if btpOperator.Status.State == v1alpha1.StateError && btpOperator.Status.Conditions[0].Reason == string(conditions.OlderCRExists) {
-				btpOperatorInErrorExists = true
-				continue
-			}
+				btpOperatorWithReadyState, btpOperatorWithErrorState := &v1alpha1.BtpOperator{}, &v1alpha1.BtpOperator{}
+				Eventually(func() (bool, error) {
+					err := k8sClient.Get(ctx, client.ObjectKey{Namespace: defaultNamespace, Name: firstBtpOperator}, btpOperatorWithReadyState)
+					return btpOperatorWithReadyState.Status.State == v1alpha1.StateReady, err
+				}).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(BeTrue())
+				Eventually(func() (bool, error) {
+					err := k8sClient.Get(ctx, client.ObjectKey{Namespace: defaultNamespace, Name: secondBtpOperator}, btpOperatorWithErrorState)
+					return btpOperatorWithErrorState.Status.State == v1alpha1.StateError && btpOperatorWithErrorState.Status.Conditions[0].Reason == string(conditions.OlderCRExists), err
+				}).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(BeTrue())
 
-			if btpOperator.Status.State == v1alpha1.StateReady {
-				btpOperatorInReadyExists = true
-				continue
-			}
-		}
-		Expect(btpOperatorInErrorExists).To(BeTrue())
-		Expect(btpOperatorInReadyExists).To(BeTrue())
+				Expect(k8sClient.Delete(ctx, btpOperator1)).To(Succeed())
+				Eventually(updateCh).Should(Receive(matchReadyCondition(v1alpha1.StateDeleting, metav1.ConditionFalse, conditions.ServiceInstancesAndBindingsNotCleaned)))
+				Expect(k8sClient.Get(ctx, client.ObjectKey{Namespace: btpOperator1.GetNamespace(), Name: btpOperator1.GetName()}, btpOperator1)).To(Succeed())
+				btpOperator1.SetFinalizers([]string{})
+				Expect(k8sClient.Update(ctx, btpOperator1)).To(Succeed())
+				Eventually(updateCh).Should(Receive(matchDeleted()))
 
-		Expect(k8sClient.Delete(ctx, btpOperator1)).To(Succeed())
-		Eventually(updateCh).Should(Receive(matchReadyCondition(v1alpha1.StateDeleting, metav1.ConditionFalse, conditions.ServiceInstancesAndBindingsNotCleaned)))
-		time.Sleep(time.Second * 5)
-		Expect(k8sClient.Get(ctx, client.ObjectKey{Namespace: btpOperator1.GetNamespace(), Name: btpOperator1.GetName()}, btpOperator1)).To(Succeed())
-		btpOperator1.SetFinalizers([]string{})
-		Eventually(func() error { return k8sClient.Update(ctx, btpOperator1) }).WithTimeout(k8sOpsTimeout).WithPolling(k8sOpsPollingInterval).Should(Succeed())
-		time.Sleep(time.Second * 5)
-		err = k8sClient.List(ctx, btpOperators)
-		Expect(err).To(BeNil())
-		Expect(len(btpOperators.Items)).To(BeEquivalentTo(1))
+				Expect(k8sClient.List(ctx, btpOperators)).To(Succeed())
+				Expect(len(btpOperators.Items)).To(BeEquivalentTo(1))
 
-		Expect(btpOperators.Items[0].Name).To(BeEquivalentTo(secondBtpOperator))
-		Expect(btpOperators.Items[0].Status.State).To(BeEquivalentTo(v1alpha1.StateReady))
-		Expect(btpOperators.Items[0].Status.Conditions[0].Reason).To(BeEquivalentTo(conditions.ReconcileSucceeded))
+				Expect(btpOperators.Items[0].Name).To(BeEquivalentTo(secondBtpOperator))
+				Expect(btpOperators.Items[0].Status.State).To(BeEquivalentTo(v1alpha1.StateReady))
+				Expect(btpOperators.Items[0].Status.Conditions[0].Reason).To(BeEquivalentTo(conditions.ReconcileSucceeded))
 
+			})
+		})
 	})
 })
