8ca7ba8905ff41e3fc32fd7e191d631c4e47fa36
Marcin Szwed <marcin.szwed@sap.com>
2023-07-14 14:10:01 +0200

    Remove unnecessary test label

--- controllers/btpoperator_controller_cr_rotation_test.go
+++ controllers/btpoperator_controller_cr_rotation_test.go
@@ -19,7 +19,7 @@ const (
 	thirdBtpOperator  = "third"
 )
 
-var _ = Describe("BTP Operator CR leader replacement", Label("debug"), func() {
+var _ = Describe("BTP Operator CR leader replacement", func() {
 
 	BeforeEach(func() {
 		GinkgoWriter.Println("--- PROCESS:", GinkgoParallelProcess(), "---")
