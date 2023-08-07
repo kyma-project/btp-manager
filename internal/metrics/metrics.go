96b9dfa4eda0edeec02d9f2747b18ebeffbfb065
Marek Michali <marek.michali@sap.com>
2023-08-07 08:16:22 +0200

    Add certs regenerations counter metric

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,18 +2,30 @@ package metrics
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 const (
 	metricsNamespace = "btpmanager"
 )
 
-type Metrics struct{}
+type Metrics struct {
+	certsRegeerationsCounter prometheus.Counter
+}
 
 func (m *Metrics) registerMetrics() {
 	//register new custom metrics here, for example:
 	//counter := prometheus.NewCounter(....)
 	//metrics.Registry.MustRegister(counter)
+	certRegenCounter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: buildMetricName("", "certs_regenerations_count"),
+	})
+	m.certsRegeerationsCounter = certRegenCounter
+	metrics.Registry.MustRegister(certRegenCounter)
+}
+
+func (m *Metrics) IncreaseCertsRegenerationsCounter() {
+	m.certsRegeerationsCounter.Inc()
 }
 
 func NewMetrics() *Metrics {
