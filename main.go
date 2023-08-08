b31dc5de120c081444f5732a132152770098a68f
ukff <110393214+ukff@users.noreply.github.com>
2023-08-08 13:24:52 +0200

    wip - temp commit msg

--- main.go
+++ main.go
@@ -1,5 +1,3 @@
-//xxx22
-//tt
 /*
 Copyright 2022.
 ai
@@ -8,14 +6,14 @@ you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
-a
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions  and
-alimitations under the License.
+under the License.
 */
-//test
+
 package main
 
 import (
