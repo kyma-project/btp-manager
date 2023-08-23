66644ef8158afa2382f58ef4e74e3a164c11d9d9
ukff <110393214+ukff@users.noreply.github.com>
2023-08-23 14:25:33 +0200

    wip - temp commit msg

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-//A
 /*
 Copyright 2022.
 
@@ -14,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions  and
 limitations under the License.
 */
-//B
 
 package main
 
