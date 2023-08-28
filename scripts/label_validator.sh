1d24b5486e3741840886fb46848acbaec4c988fe
ukff <110393214+ukff@users.noreply.github.com>
2023-08-28 14:19:44 +0200

    wip - temp commit msg

--- scripts/label_validator.sh
+++ scripts/label_validator.sh
@@ -133,7 +133,7 @@ function runOnPr() {
     response=$(curl -L \
             -X POST \
             -H "Accept: application/vnd.github+json" \
-            -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+            -H "Authorization: Bearer ${BOT_TOKEN}" \
             -H "X-GitHub-Api-Version: 2022-11-28" \
             https://api.github.com/repos/$GITHUB_ORG/btp-manager/issues/${PR_ID}/comments \
             -d "$payload")
