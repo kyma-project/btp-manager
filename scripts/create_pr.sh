1609ff1c90fbd7371de433b53fafdc8e4d24100a
Marek Michali <marek.michali@sap.com>
2023-09-04 09:06:28 +0200

    wip

--- scripts/create_pr.sh
+++ scripts/create_pr.sh
@@ -16,22 +16,29 @@ set -o pipefail # prevents errors in a pipeline from being masked# link the PR f
 #   BRANCH_NAME                   - branch with updated resources
 #   TAG                           - new chart version
 
-TAG=$1
-
+# add changed files to stage
 git add sec-scanners-config.yaml
+
+#stash staged changes
 git stash push --staged
+
+#pass changes to branch created from main
 git checkout --force -B main refs/remotes/origin/main
-git checkout -B sec_config_${TAG}
+git checkout -B ${BRANCH_NAME}
+
+#apply stashed changes
 git stash apply
 git add sec-scanners-config.yaml
-git config --global user.email marek.michali@sap.com
-git config --global user.name MarekMichali
+
+#configure git
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_NAME}
+
+#commit and push changes
 git commit -m "Test commit"
-echo "remote"
-git remote set-url origin https://x-access-token:${GH_TOKEN}@github.com/MarekMichali/btp-manager.git
-echo "push"
-git push --set-upstream origin sec_config_${TAG} -f
-echo "pr"
-resp=$(gh pr create -B main --title "Test commit" --body "Test body" | tail -n 1)
-echo "resp"
-echo $resp
+git remote set-url origin https://x-access-token:${GH_TOKEN}@github.com/${KYMA_BTP_MANAGER_REPO}.git
+git push --set-upstream origin ${BRANCH_NAME} -f
+
+#create PR
+pr_link=$(gh pr create -B main --title "Test commit" --body "Test body" | tail -n 1)
+echo "Link for created PR: ${pr_link}"
