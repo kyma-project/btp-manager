bc133a0c0c8ea111ca5a84db68c87be72c087494
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-09-06 13:10:48 +0200

    Add additional scan config option to script (#429)
    * Add additional scan config option to script
    
    * Cosmetic change

--- scripts/create_scan_config.sh
+++ scripts/create_scan_config.sh
@@ -3,9 +3,12 @@
 # This script has the following arguments:
 #                       binary image reference (mandatory)
 #                       filename of file to be created (optional)
-# ./create_scan_config image temp_scan_config.yaml
+#                       release tag (optional)
+# ./create_scan_config image temp_scan_congitfig.yaml          - use when building module image
+# ./create_scan_config image temp_scan_congitfig.yaml tag      - use when bumping the config on the main branch
 
 FILENAME=${2-../sec-scanners-config.yaml}
+TAG=${3:-}
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -13,19 +16,31 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being maskedPORT=5001
 
-# Expected variables passed (passed from CI):
-#   IMAGE_REPO                     - Repository with image to be scanned
-
-TAG=${1}
+IMAGE=${1}
 echo "Creating security scan configuration file:"
-cat <<EOF | tee ${FILENAME}
+
+# add rc-tag when creating the config on the main branch
+if [ -n "${TAG}" ]; then
+  cat <<EOF | tee ${FILENAME}
 module-name: btp-operator
 rc-tag: ${TAG}
 protecode:
-  - ${IMAGE_REPO}:${TAG}
+  - ${IMAGE}
 whitesource:
   language: golang-mod
   subprojects: false
   exclude:
     - "**/*_test.go"
 EOF
+else
+  cat <<EOF | tee ${FILENAME}
+module-name: btp-operator
+protecode:
+  - ${IMAGE}
+whitesource:
+  language: golang-mod
+  subprojects: false
+  exclude:
+    - "**/*_test.go"
+EOF
+fi
