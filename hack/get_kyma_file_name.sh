da378f4c1be8698155526b0288c59eb90f32a354
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2023-09-06 10:02:57 +0200

    Kyma CLI for arm64 added (#425)
    arm64 build added

--- hack/get_kyma_file_name.sh
+++ hack/get_kyma_file_name.sh
@@ -9,7 +9,8 @@ function get_kyma_file_name () {
 	[ "$_OS_TYPE" == "Linux"   ] && [ "$_OS_ARCH" == "arm64"  ] && echo "kyma-linux-arm" ||
 	[ "$_OS_TYPE" == "Windows" ] && [ "$_OS_ARCH" == "x86_64" ] && echo "kyma.exe"       ||
 	[ "$_OS_TYPE" == "Windows" ] && [ "$_OS_ARCH" == "arm64"  ] && echo "kyma-arm.exe"   ||
-	[ "$_OS_TYPE" == "Darwin"  ] && [ "$_OS_ARCH" == "x86_64" ] && echo "kyma-darwin"
+	[ "$_OS_TYPE" == "Darwin"  ] && [ "$_OS_ARCH" == "x86_64" ] && echo "kyma-darwin"    ||
+	[ "$_OS_TYPE" == "Darwin"  ] && [ "$_OS_ARCH" == "arm64"  ] && echo "kyma-darwin-arm"
 }
 
 get_kyma_file_name "$@"
