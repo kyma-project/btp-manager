d07b449bb30e514212294c1b7cbaf06007bdf2ef
ukff <110393214+ukff@users.noreply.github.com>
2023-12-14 14:03:48 +0100

    wip - temp commit msg

--- scripts/testing/check_top.sh
+++ scripts/testing/check_top.sh
@@ -5,23 +5,21 @@ set -o nounset  # treat unset variables as an error and exit immediately.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
-TIMEOUT=30
-NEXT_TRY_WAIT=5
+function measure_pod_resources() {
+     TIMEOUT=30
+     NEXT_TRY_WAIT=5
+     LABEL_SELECTOR=$1
+
+     SECONDS=0
+     while (($SECONDS < $TIMEOUT )); do
+          kubectl top pod -l "$LABEL_SELECTOR" -n kyma-system --containers
+          [ $? == 0 ] && break
+          sleep $NEXT_TRY_WAIT
+     done
+}
 
 echo -e "\n--- BTP Manager checking kubectl top" 
-
-SECONDS=0
-while ((SECONDS < $TIMEOUT )); do
-     kubectl top pod -l app.kubernetes.io/component=btp-manager.kyma-project.io -n kyma-system --containers
-     [ $? == 0 ] && break
-     sleep $NEXT_TRY_WAIT
-done
+measure_pod_resources app.kubernetes.io/component=btp-manager.kyma-project.io
 
 echo -e "\n--- BTP Operator checking kubectl top" 
-
-SECONDS=0
-while ((SECONDS < $TIMEOUT )); do
-     kubectl top pod -l app.kubernetes.io/name=sap-btp-operator -n kyma-system --containers
-     [ $? == 0 ] && break
-     sleep $NEXT_TRY_WAIT
-done
+measure_pod_resources app.kubernetes.io/name=sap-btp-operator
