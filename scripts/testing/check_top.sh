81615af589aa822ff9651651613c4d0561ea1e48
ukff <110393214+ukff@users.noreply.github.com>
2023-12-14 13:41:29 +0100

    wip - temp commit msg

--- scripts/testing/check_top.sh
+++ scripts/testing/check_top.sh
@@ -2,12 +2,24 @@
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
-set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
-echo -e "\n--- BTP Manager checking kubectl top"
-kubectl top pod -l app.kubernetes.io/component=btp-manager.kyma-project.io -n kyma-system --containers
+TIMEOUT=30
+NEXT_TRY_WAIT=5
 
-echo -e "\n--- BTP Operator checking kubectl top"
-kubectl top pod -l app.kubernetes.io/name=sap-btp-operator -n kyma-system --containers
+echo -e "\n--- BTP Manager checking kubectl top" 
+SECONDS=0
+while ((SECONDS < $TIMEOUT )); do
+     kubectl top pod -l app.kubernetes.io/component=btp-manager.kyma-project.io -n kyma-system --containers
+     [ $? == 0 ] && break
+     sleep $NEXT_TRY_WAIT
+done
+
+echo -e "\n--- BTP Operator checking kubectl top" 
+SECONDS=0
+while ((SECONDS < $TIMEOUT )); do
+     kubectl top pod -l app.kubernetes.io/name=sap-btp-operator -n kyma-system --containers
+     [ $? == 0 ] && break
+     sleep $NEXT_TRY_WAIT
+done
