2c682bd08a560a2d316da0132f859b53a8633a38
kyma-btp-manager-serviceuser <jaroslaw.pieszka@sap.com>
2024-01-02 15:23:29 +0100

    remove dumping btp-operator json

--- scripts/testing/install_module.sh
+++ scripts/testing/install_module.sh
@@ -49,7 +49,7 @@ echo -e "\n---Installing BTP operator"
 kubectl apply -f ${YAML_DIR}/e2e-test-btpoperator.yaml
 
 while [[ $(kubectl get btpoperators/e2e-test-btpoperator -ojson| jq '.status.conditions[] | select(.type=="Ready") |.status+.reason'|xargs)  != "TrueReconcileSucceeded" ]];
-do echo -e "\n---Waiting for BTP Operator to be ready and reconciled"; sleep 5; kubectl get btpoperators/e2e-test-btpoperator -ojson; sleep 5 ; done
+do echo -e "\n---Waiting for BTP Operator to be ready and reconciled"; sleep 5; done
 
 # verifying whether service instance and service binding resources were created
 echo -e "\n---Checking if serviceinstances and servicebindings CRDs are created"
