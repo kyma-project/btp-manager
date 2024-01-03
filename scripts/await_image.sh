afb1cc80b2ca421c8a8a061c91c5eb169c7311df
kyma-btp-manager-serviceuser <jaroslaw.pieszka@sap.com>
2024-01-03 17:51:50 +0100

    await script renamed

--- scripts/await_image.sh
+++ scripts/await_image.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-SKIP_ASSETS=$2
-
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
@@ -10,9 +8,8 @@ set -o pipefail # prevents errors in a pipeline from being masked
 
 # This script has the following arguments:
 #                       - BTP Manager binary image tag - mandatory
-#                       --skip-template - optional
 #
-# ./await_artifacts.sh 1.1.0
+# ./await_image.sh 1.1.0
 
 # Expected variables:
 #             BTP_MANAGER_REPO - btp-operator binary image repository
@@ -22,20 +19,6 @@ set -o pipefail # prevents errors in a pipeline from being masked
 export IMAGE_TAG=$1
 
 PROTOCOL=docker://
-
-RELEASES_URL="https://api.github.com/repos/kyma-project/btp-manager/releases"
-ARTIFACTS_REGEX="(btp-manager.yaml|template.yaml|template_control_plane.yaml)"
-
-if [ "${SKIP_ASSETS}" != "--skip-templates" ]
-then
-  echo "Finding assets for: ${IMAGE_TAG}"
-  # all 3  artifacts available?
-  until [ $(curl -sL -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" ${RELEASES_URL} | jq '.[] | select(.tag_name == env.IMAGE_TAG) | .assets[] | .browser_download_url | split("/") | last ' | sort -u | grep -Ec ${ARTIFACTS_REGEX}) -eq 3 ]; do
-    echo 'waiting for the assets'
-    sleep 10
-  done
-  echo "assets available"
-fi
 
 until $(skopeo list-tags ${PROTOCOL}${BTP_MANAGER_REPO} | jq '.Tags|any(. == env.IMAGE_TAG)'); do
   echo "Waiting for BTP Manager binary image: ${BTP_MANAGER_REPO}:${IMAGE_TAG}"
