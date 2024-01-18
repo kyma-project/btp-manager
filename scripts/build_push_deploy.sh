6b5011487767fc234b01c7fe49f07c6c522fadf9
kyma-btp-manager-serviceuser <jaroslaw.pieszka@sap.com>
2024-01-18 15:13:54 +0100

    build push deploy sequence in the script

--- scripts/build_push_deploy.sh
+++ scripts/build_push_deploy.sh
@@ -4,9 +4,7 @@
 #                       LOCAL_REGISTRY
 #                       K3D_REGISTRY
 #                       PR_NAME
-#                       MODULE_PREFIX
-# and returns local reference to the created module to stdout
-# ./build_and_push_locally.sh
+# ./build_push_deploy.sh
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -17,10 +15,19 @@ set -o pipefail # prevents errors in a pipeline from being maskedPORT=5001
 # registry access from local machine
 LOCAL_REGISTRY=${LOCAL_REGISTRY:-localhost:5001}
 
+#registry access from k3d cluster
+K3D_REGISTRY=${K3D_REGISTRY:-k3d-kyma-registry:5000}
+
 PR_NAME=${PR_NAME:-PR-undefined}
 IMG_NAME=btp-manager:${PR_NAME}
 
 echo "Creating binary image and pushing to registry: ${LOCAL_REGISTRY}"
-make module-image LOCAL_REGISTRY=${LOCAL_REGISTRY} IMG=${LOCAL_REGISTRY}/${IMG_NAME}
+make module-image IMG=${LOCAL_REGISTRY}/${IMG_NAME}
+
+echo "Creating prerequisites"
+kubectl apply -f deployments/prerequisites.yaml
+
+echo "Deploying binary image from local registry: ${K3D_REGISTRY}"
+make deploy IMG=${K3D_REGISTRY}/${IMG_NAME}
 
 
