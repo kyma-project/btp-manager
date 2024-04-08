88b8a5d0bbc75e2c9fbaf0261a9d5db3a45a39c5
Marcin Szwed <marcin.szwed@sap.com>
2024-04-08 12:31:29 +0200

    Interface segregation

--- internal/cluster-object/object_provider.go
+++ internal/cluster-object/object_provider.go
@@ -4,7 +4,17 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type Provider interface {
-	All() (client.ObjectList, error)
-	ByNameAndNamespace(name, namespace string) (client.Object, error)
+const logComponentNameKey = "component"
+
+type Provider[T client.Object | client.ObjectList] interface {
+	ClusterScopedProvider[T]
+	NamespacedProvider[T]
+}
+
+type ClusterScopedProvider[T client.ObjectList] interface {
+	All() (T, error)
+}
+
+type NamespacedProvider[T client.Object] interface {
+	GetByNameAndNamespace(name, namespace string) (T, error)
 }
