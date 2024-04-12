04de96c1d662c4794749c792c4f23af0dbf499b9
Marcin Szwed <marcin.szwed@sap.com>
2024-04-12 12:11:44 +0200

    Add NamespaceProvider unit tests

--- internal/cluster-object/namespace_provider_test.go
+++ internal/cluster-object/namespace_provider_test.go
@@ -1,1 +1,71 @@
 package clusterobject
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestNamespaceProvider(t *testing.T) {
+	// given
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	t.Run("should fetch all namespaces", func(t *testing.T) {
+		// given
+		namespaces := initNamespaces()
+		k8sClient := fake.NewClientBuilder().WithLists(namespaces).Build()
+		nsProvider := NewNamespaceProvider(k8sClient, logger)
+
+		// when
+		nsList, err := nsProvider.All(context.TODO())
+
+		// then
+		if err != nil {
+			t.Errorf("Error while fetching namespaces: %s", err)
+		}
+		if len(nsList.Items) != 3 {
+			t.Errorf("Expected 3 namespaces, got %d", len(nsList.Items))
+		}
+	})
+
+	t.Run("should return error when no namespaces found", func(t *testing.T) {
+		// given
+		k8sClient := fake.NewClientBuilder().Build()
+		nsProvider := NewNamespaceProvider(k8sClient, logger)
+
+		// when
+		_, err := nsProvider.All(context.TODO())
+
+		// then
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+}
+
+func initNamespaces() *corev1.NamespaceList {
+	return &corev1.NamespaceList{
+		Items: []corev1.Namespace{
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "kube-system",
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "default",
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+			},
+		},
+	}
+}
