85cdcf9e9cc97acf91d46d2fc0460787b4fa2f8a
L. Jezak <110393214+ukff@users.noreply.github.com>
2024-05-29 14:26:11 +0200

    Service Manager UI (#649)

---
+++ ui/src/shared/models.tsx
@@ -0,0 +1,55 @@
+export interface Secrets {
+  items: Secret[];
+}
+
+export interface Secret {
+  name: string;
+  namespace: string;
+}
+
+export interface ServiceOfferings {
+  numItems: number;
+  items: ServiceOffering[];
+}
+
+export interface ServiceOffering {
+  id: string;
+  description: string;
+  catalogId: string;
+  catalogName: string;
+  metadata: ServiceOfferingMetadata;
+}
+
+export interface ServiceOfferingMetadata {
+  imageUrl: string;
+  displayName: string;
+}
+
+export interface ServiceOfferingDetails {
+  longDescription: string;
+}
+
+export interface ServiceInstances {
+  items: ServiceInstance[];
+}
+
+export interface ServiceInstance {
+  id: string;
+  name: string;
+  context: string[];
+  namespace: string;
+  serviceBindings: ServiceInstanceBindings[];
+}
+
+export interface ServiceInstanceBindings {
+  id: string;
+  name: string;
+  namespace: string;
+}
+
+export interface ServiceInstanceDetails {
+  id: string;
+  name: string;
+  context: string;
+  namespace: string;
+}
