e36a69fb19a8cb6cbe02e23392cee794e5f9bbea
ukff <110393214+ukff@users.noreply.github.com>
2024-06-11 16:42:50 +0200

    wip

--- ui/src/components/ServiceOfferingsView.tsx
+++ ui/src/components/ServiceOfferingsView.tsx
@@ -5,30 +5,26 @@ import axios from "axios";
 import {ServiceOfferingDetails, ServiceOfferings} from "../shared/models";
 import api from "../shared/api";
 import "@ui5/webcomponents-icons/dist/AllIcons.js"
+import "@ui5/webcomponents-fiori/dist/illustrations/NoEntries.js"
 import Ok from "../shared/validator";
 
 function ServiceOfferingsView(props: any) {
     const [offerings, setOfferings] = useState<ServiceOfferings>();
-    const [offeringsCachced, setOfferingsCached] = useState<ServiceOfferings>();
-    const [serviceOfferingDetails, setServiceOfferingDetails] =
-        useState<ServiceOfferingDetails>();
+    const [serviceOfferingDetails, setServiceOfferingDetails] = useState<ServiceOfferingDetails>();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [labels, setLabels] = useState<JSX.Element>();
     const dialogRef = useRef(null);
-    const [planDesc, setPlanDesc] = useState<string>();
+    
     const handleOpen = (id: any) => {
         // @ts-ignore
         dialogRef.current.show();
-        console.log("handleOpen id: ", id)
         load(id);
     };
     const handleClose = () => {
         // @ts-ignore
         dialogRef.current.close();
     };
-
-
+    
     useEffect(() => {
         if (props.secret == null) {
             return;
@@ -51,33 +47,16 @@ function ServiceOfferingsView(props: any) {
                     setError(error);
                 });
         }
+        setLoading(false);
     }, []);
 
 
     if (loading) {
-        return <ui5.Loader progress="60%"/>
+        return <ui5.Loader progress="100%"/>
     }
 
     if (error) {
         return <ui5.IllustratedMessage name="UnableToLoad" style={{height: "50vh", width: "30vw"}}/>
-    }
-
-    function filter(e: string) {
-        return;
-        console.log("filtering")
-        // @ts-ignore
-        setOfferings(offeringsCachced);
-        const ServiceOfferings = offeringsCachced;
-        let ii = 0;
-        // @ts-ignore
-        for (let i = 0; i < offerings?.items.length; i++) {
-            // @ts-ignore
-            if (offerings?.items[i].metadata.displayName.includes(e)) {
-                // @ts-ignore
-                ServiceOfferings.items[ii] = [offerings?.items[i]];
-                ii++;
-            }
-        }
     }
 
     function getImg(b64: string) {
@@ -89,19 +68,16 @@ function ServiceOfferingsView(props: any) {
     }
 
     function load(id: string) {
-        console.log("loading id: ", id)
         setLoading(true);
         axios
             .get<ServiceOfferingDetails>(api(`service-offering-details/${id}`))
             .then((response) => {
-                console.log("load response: ", response)
                 setLoading(false);
                 setServiceOfferingDetails(response.data);
             })
             .catch((error) => {
                 setLoading(false);
                 setError(error);
-                console.log("load error: ", error)
             });
         setLoading(false);
     }
@@ -111,11 +87,7 @@ function ServiceOfferingsView(props: any) {
         if (!Ok(offerings) || !Ok(offerings.items)) {
             return <ui5.IllustratedMessage name="NoEntries" style={{height: "50vh", width: "30vw"}}/>
         }
-        //console.log("renderData")
-        //filter(props.phrase)
-
         return offerings?.items.map((offering, index) => {
-            // @ts-ignore
             // @ts-ignore
             return (
                 <>
@@ -124,10 +96,8 @@ function ServiceOfferingsView(props: any) {
                         style={{
                             width: "20%",
                             height: "0",
-                            padding: "10px"
                         }}
                         onClick={() => {
-                            console.log("opening id: ", offering.id)
                             handleOpen(offering.id);
                         }}
                         header={
@@ -137,7 +107,7 @@ function ServiceOfferingsView(props: any) {
                                         <img alt="" src={getImg(offering.metadata.imageUrl)}></img>
                                     </ui5.Avatar>
                                 }
-                                subtitleText={offering.metadata.displayName}
+                                subtitleText={offering.description}
                                 titleText={offering.catalogName}
                                 status={formatStatus(index, offerings?.numItems)}
                                 interactive
@@ -159,103 +129,52 @@ function ServiceOfferingsView(props: any) {
                                         }
                                     />
                                 }
-                                onAfterClose={function _a() {
-                                }}
-                                onAfterOpen={function _a() {
-                                }}
-                                onBeforeClose={function _a() {
-                                }}
-                                onBeforeOpen={function _a() {
-                                }}
                             >
-                                <div>
                                     <ui5.Panel
                                         headerLevel="H2"
                                         headerText="Service Offering Details"
                                     >
                                         <ui5.Text>{serviceOfferingDetails?.longDescription}</ui5.Text>
                                     </ui5.Panel>
-                                </div>
+                                
+                                    <ui5.Panel
+                                        headerLevel="H2"
+                                        headerText="Plan Details"
+                                    >
+                                        <ui5.Form>
+                                            <ui5.FormItem label="Plan Name">
+                                                <ui5.Select>
+                                                    {
+                                                        serviceOfferingDetails?.plans.map((plan, index) =>
+                                                            (
+                                                                <ui5.Option
+                                                                    key={index}>{plan.name}
+                                                                </ui5.Option>
+                                                            ))
+                                                    }
+                                                </ui5.Select>
+                                            </ui5.FormItem>
+                                        </ui5.Form>
+                                    </ui5.Panel>
 
                                 <ui5.Panel
                                     accessibleRole="Form"
                                     headerLevel="H2"
                                     headerText="Create Service Instance"
-                                    onToggle={function _a() {
-                                    }}
                                 >
                                     <ui5.Form>
                                         <ui5.FormItem label="Name">
                                             <ui5.Input></ui5.Input>
                                         </ui5.FormItem>
                                         <ui5.FormItem label="External Name">
-                                            <ui5.Input></ui5.Input>
+                                        <ui5.Input></ui5.Input>
                                         </ui5.FormItem>
                                         <ui5.FormItem label="Provisioning Parameters">
                                             <ui5.TextArea
                                                 style={{width: "100%"}}
-                                                onChange={function _a() {
-                                                }}
-                                                onInput={function _a() {
-                                                }}
-                                                onScroll={function _a() {
-                                                }}
-                                                onSelect={function _a() {
-                                                }}
                                                 valueState="None"
                                                 title="Provisioning Parameters"
                                             />
-                                        </ui5.FormItem>
-                                        <ui5.FormItem label="Plan Name">
-                                            <ui5.Select
-                                            >
-                                                {
-                                                    serviceOfferingDetails?.plans.map((plan, index) =>
-                                                        (
-                                                            <ui5.Option onLoad={() => {
-                                                                console.log("onLoad")
-                                                                setPlanDesc(plan.description);
-                                                            }} onClick={() => {
-                                                                console.log("onClick")
-                                                            }} onFocus={() => {
-                                                                console.log("onFocus")
-                                                            }}
-                                                                        key={index}>{plan.name} X
-                                                            </ui5.Option>
-                                                        ))}
-                                            </ui5.Select>
-                                            <ui5.Text>{planDesc}</ui5.Text>
-                                        </ui5.FormItem>
-                                        <ui5.Icon name="add"
-                                                  onClick={() => {
-                                                      const it = (
-                                                          <ui5.FormItem>
-                                                              <ui5.Input style={{width: "50px"}}></ui5.Input>
-                                                              <ui5.Input style={{width: "50px"}}></ui5.Input>
-                                                          </ui5.FormItem>
-                                                      );
-                                                      // @ts-ignore
-                                                      setLabels([...labels, it]);
-                                                  }}
-                                        />
-                                        <ui5.FormItem label="Labels">{labels}</ui5.FormItem>
-                                        <ui5.FormItem label="Annotations">
-                                            <ui5.Icon name="add"/>
-                                            <ui5.FormItem>
-                                                <ui5.Input style={{width: "50px"}}></ui5.Input>
-                                            </ui5.FormItem>
-                                            <ui5.FormItem>
-                                                <ui5.Input style={{width: "50px"}}></ui5.Input>
-                                            </ui5.FormItem>
-                                        </ui5.FormItem>
-                                        <ui5.FormItem>
-                                            <ui5.Button
-                                                style={{width: "100px"}}
-                                                onClick={function _a() {
-                                                }}
-                                            >
-                                                Create
-                                            </ui5.Button>
                                         </ui5.FormItem>
                                     </ui5.Form>
                                 </ui5.Panel>
--- ui/src/components/View.tsx
+++ ui/src/components/View.tsx
@@ -11,10 +11,6 @@ function Overview(props: any) {
   function handler(e: any) {
     setSecret(e);
   }
-  function handler2(e: any) {
-      console.log("handler2")
-      setPhrase(e);
-  }
   
   return (
     <>
@@ -29,14 +25,6 @@ function Overview(props: any) {
         startContent={<span>Select your credentials:</span>}
       >
         <Secrets handler={(e: any) => handler(e)} style={{ width: "100vw" }} />
-          <ui5.TextArea
-              onInput={function _a(event: ui5.Ui5CustomEvent<ui5.TextAreaDomRef, never>){
-                    console.log("onInput")
-                    const phrase=event.target.value;
-                    handler2(phrase);
-              }}
-              valueState="None"
-          />
       </ui5.Bar>
       <>
           <div>
@@ -54,14 +42,12 @@ function Overview(props: any) {
                   >
                       <ui5.SideNavigationItem
                           text="Marketplace"
-                          icon="home"
                           onClick={() => {
                               setPageContent(<ServiceOfferings secret={secret} phrase={phrase}/>);
                           }}
                       />
                       <ui5.SideNavigationItem
                           text="Service Instances"
-                          icon="home"
                           onClick={() => {
                               setPageContent(<ServiceInstancesView/>);
                           }}
