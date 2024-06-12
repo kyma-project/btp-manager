9f4b497b68614e0acc78ffb5d1272cf779d8e26e
Marcin Szwed <marcin.szwed@sap.com>
2024-06-12 14:32:17 +0200

    Change metrics server port

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ func main() {
 
 func parseCmdFlags(probeAddr *string, metricsAddr *string, enableLeaderElection *bool) {
 	flag.StringVar(probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.StringVar(metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(metricsAddr, "metrics-bind-address", ":9090", "The address the metric endpoint binds to.")
 	flag.BoolVar(enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
