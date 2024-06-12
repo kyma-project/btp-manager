8689c95e23c44b47c8cc7f5a161235c6ced2d190
Marek Michali <marek.michali@sap.com>
2024-06-12 12:58:39 +0200

    Better logs

--- scripts/testing/multiple_btpoperators_exist.sh
+++ scripts/testing/multiple_btpoperators_exist.sh
@@ -1,55 +1,62 @@
 #!/usr/bin/env bash
 
+# This script has the following arguments:
+#     - number of iterations of switching between which btpOperator is reconciled
+# ./multiple_btpoperators_exist.sh 10
+
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
+iterations=$1
+YAML_DIR="scripts/testing/yaml"
+
 apply_btp_operator() {
     local namespace=$1
-    kubectl apply -f https://github.com/kyma-project/btp-manager/releases/latest/download/btp-operator-default-cr.yaml -n $namespace
+    echo -e "\n---Creating BTP Operator in $namespace namespace"
+    kubectl apply -f ${YAML_DIR}/e2e-test-btpoperator.yaml -n $namespace
 }
 
 wait_for_condition() {
     local condition=$1
     local namespace=$2
     local message=$3
-    while [[ $(kubectl get btpoperators btpoperator -n $namespace -ojson| jq '.status.conditions[] | select(.type=="Ready") |.status+.reason'|xargs)  != $condition ]];
+    while [[ $(kubectl get btpoperators/e2e-test-btpoperator -n $namespace -ojson| jq '.status.conditions[] | select(.type=="Ready") |.status+.reason'|xargs)  != $condition ]];
     do
         echo -e "\n---Waiting for BTP Operator in $namespace namespace to be $message"; sleep 5;
     done
 }
 
-delete_and_patch_btp_operator() {
+delete_btp_operator() {
     local namespace=$1
-    kubectl delete btpoperators btpoperator -n $namespace &
-    kubectl patch btpoperator btpoperator -n $namespace -p '{"metadata":{"finalizers":[]}}' --type=merge &
+    echo -e "\n---Deleting BTP Operator in $namespace namespace"
+    kubectl delete btpoperators e2e-test-btpoperator -n $namespace &
+    kubectl patch btpoperator e2e-test-btpoperator -n $namespace -p '{"metadata":{"finalizers":[]}}' --type=merge &
     wait
 }
 
-iterations=$1
-
-kubectl apply -f deployments/prerequisites.yaml  
-kubectl apply -f examples/btp-manager-secret.yaml
-kubectl apply -f https://github.com/kyma-project/btp-manager/releases/latest/download/btp-manager.yaml
-apply_btp_operator "default"
-wait_for_condition "TrueReconcileSucceeded" "default" "ready and reconciled"
+#kubectl apply -f deployments/prerequisites.yaml  
+#kubectl apply -f examples/btp-manager-secret.yaml
+#kubectl apply -f https://github.com/kyma-project/btp-manager/releases/latest/download/btp-manager.yaml
+#apply_btp_operator "default"
+#wait_for_condition "TrueReconcileSucceeded" "default" "ready and reconciled"
 
 export SI_NAME="test"
-envsubst < scripts/testing/yaml/e2e-test-service-instance.yaml | kubectl apply -f -
+envsubst < ${YAML_DIR}/e2e-test-service-instance.yaml | kubectl apply -f -
 export SB_NAME="test"
-envsubst < scripts/testing/yaml/e2e-test-service-binding.yaml | kubectl apply -f -
+envsubst < ${YAML_DIR}//e2e-test-service-binding.yaml | kubectl apply -f -
 
 for ((i=1; i<=$iterations; i++))
 do        
     apply_btp_operator "kyma-system"
     wait_for_condition "FalseOlderCRExists" "kyma-system" "ready and in error OlderCRExists"
-    delete_and_patch_btp_operator "default"
+    delete_btp_operator "default"
     wait_for_condition "TrueReconcileSucceeded" "kyma-system" "ready and reconciled"
     apply_btp_operator "default"
     wait_for_condition "FalseOlderCRExists" "default" "ready and in error OlderCRExists"
-    delete_and_patch_btp_operator "kyma-system"
+    delete_btp_operator "kyma-system"
     wait_for_condition "TrueReconcileSucceeded" "default" "ready and reconciled"
 done
 
