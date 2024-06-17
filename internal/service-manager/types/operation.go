cc938ae693d366aa7eee1d4fe25dcf9ad2c316f4
Marcin Szwed <marcin.szwed@sap.com>
2024-06-17 13:58:49 +0200

    Add Operation struct

---
+++ internal/service-manager/types/operation.go
@@ -0,0 +1,40 @@
+package types
+
+import "encoding/json"
+
+const AddLabelOperation = "add"
+const RemoveLabelOperation = "remove"
+
+type LabelChange struct {
+	Operation string   `json:"op"`
+	Key       string   `json:"key"`
+	Values    []string `json:"values"`
+}
+
+type OperationCategory string
+
+const (
+	CREATE OperationCategory = "create"
+	UPDATE OperationCategory = "update"
+	DELETE OperationCategory = "delete"
+)
+
+type OperationState string
+
+const (
+	PENDING    OperationState = "pending"
+	SUCCEEDED  OperationState = "succeeded"
+	INPROGRESS OperationState = "in progress"
+	FAILED     OperationState = "failed"
+)
+
+const ResourceOperationsURL = "/operations"
+
+type Operation struct {
+	Common
+	Type         OperationCategory `json:"type,omitempty" yaml:"type,omitempty"`
+	State        OperationState    `json:"state,omitempty" yaml:"state,omitempty"`
+	ResourceID   string            `json:"resource_id,omitempty" yaml:"resource_id,omitempty"`
+	ResourceType string            `json:"resource_type,omitempty" yaml:"resource_type,omitempty"`
+	Errors       json.RawMessage   `json:"errors,omitempty" yaml:"errors,omitempty"`
+}
