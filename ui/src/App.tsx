51ab9e3764fb6a62c4b8485201124fa3a574be05
ukff <110393214+ukff@users.noreply.github.com>
2024-06-18 17:05:28 +0200

    wip

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css';
 import View from "./components/View";
 
 function App() {
-  return (
-      <body className="App">
-        <View />
-      </body>
-  );
-}
-
+    return (
+        <div className="App">
+            <body className="ui5-content-density-compact">
+                <View/>
+            </body>
+        </div>)
+};
 export default App;
--- ui/src/components/ServiceOfferingsView.tsx
+++ ui/src/components/ServiceOfferingsView.tsx
@@ -9,41 +9,17 @@ import "@ui5/webcomponents-fiori/dist/illustrations/AllIllustrations.js"
 import "@ui5/webcomponents-fiori/dist/illustrations/NoData.js";
 import Ok from "../shared/validator";
 import {createPortal} from "react-dom";
+import ServiceOfferingsDetailsView from "./ServiceOfferingsDetailsView";
 
 function ServiceOfferingsView(props: any) {
     const [offerings, setOfferings] = useState<ServiceOfferings>();
-    const [selectedOffering, setSelectedOffering] = useState<ServiceOffering>();
-    const [details, setDetails] = useState<ServiceOfferingDetails>();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [plan, setPlan] = useState<ServiceOfferingPlan>();
     const [dialogIsOpen, setDialogIsOpen] = useState(false);
     const dialogRef = useRef(null);
-
-    const handleOpen = (offering: ServiceOffering) => {
-        // @ts-ignore
-        load(offering);
-        setDialogIsOpen(true);
-        // @ts-ignore
-        dialogRef.current.show();
-    };
-
-    const handleClose = () => {
-        // @ts-ignore
-        setDialogIsOpen(false);
-        // @ts-ignore
-        dialogRef.current.close();
-    };
-
-    const onChangeSelect = (e: any) => {
-        // @ts-ignore
-        for (let i = 0; i < details?.plans.length; i++) {
-            if (details?.plans[i].name === e.detail.selectedOption.dataset.id) {
-                setPlan(details?.plans[i]);
-            }
-        }
-    }
-
+    const [portal, setPortal] = useState<JSX.Element>();
+    
     useEffect(() => {
         if (!Ok(props.secret)) {
             return;
@@ -78,20 +54,8 @@ function ServiceOfferingsView(props: any) {
         }
     }
 
-    function load(offering: ServiceOffering) {
-        setLoading(true);
-        axios
-            .get<ServiceOfferingDetails>(api(`service-offering/${offering.id}`))
-            .then((response) => {
-                setLoading(false);
-                setDetails(response.data);
-                setSelectedOffering(offering);
-            })
-            .catch((error) => {
-                setLoading(false);
-                setError(error);
-            });
-        setLoading(false);
+    function handleOpen(offering: ServiceOffering) {
+        
     }
 
     const renderData = () => {
@@ -117,7 +81,7 @@ function ServiceOfferingsView(props: any) {
                             width: '600px',
                         }}
                         onClick={() => {
-                            handleOpen(offering);
+                            setPortal(createPortal( <ServiceOfferingsDetailsView offering={offering} />, document.body, ""))
                         }}
                         header={
                             <ui5.CardHeader
@@ -139,121 +103,13 @@ function ServiceOfferingsView(props: any) {
                     >
                     </ui5.Card>
 
-                    {createPortal(
-                        <ui5.Dialog
-                            style={{width: "50%"}}
-                            ref={dialogRef}
-                            header={
-                                <ui5.Bar
-                                    design="Header"
-                                    startContent={
-                                        <>
-                                            <ui5.Title level="H5">Create {selectedOffering?.catalogName} Service
-                                                Instance
-                                            </ui5.Title>
-                                        </>
-                                    }
-                                />
-                            }
-                            footer={
-                                <ui5.Bar
-                                    design="Footer"
-                                    endContent={
-                                        <>
-                                            <ui5.Button>Create</ui5.Button>
-                                            <ui5.Button onClick={handleClose}>Close</ui5.Button>
-                                        </>
-                                    }
-                                />
-                            }
-                        >
-                            <ui5.Panel
-                                headerLevel="H2"
-                                headerText="Service Details"
-                            >
-                                <ui5.Form>
-                                    <ui5.FormItem label="Catalog Name">
-                                        <ui5.Text>{selectedOffering?.catalogName}</ui5.Text>
-                                    </ui5.FormItem>
-                                    <ui5.FormItem label="Description">
-                                        <ui5.Text>{selectedOffering?.description}</ui5.Text>
-                                    </ui5.FormItem>
-                                    {Ok(details?.longDescription) && <ui5.FormItem label="Long Description">
-                                        <ui5.Text>{details?.longDescription}</ui5.Text>
-                                    </ui5.FormItem>}
-                                    {Ok(selectedOffering?.metadata.supportUrl) && <ui5.FormItem label="Support URL">
-                                        <ui5.Link target="_blank" href={selectedOffering?.metadata.supportUrl}>Link
-                                        </ui5.Link>
-                                    </ui5.FormItem>}
-                                    {Ok(selectedOffering?.metadata.documentationUrl) &&
-                                        <ui5.FormItem label="Documentation URL">
-                                            <ui5.Link target="_blank"
-                                                      href={selectedOffering?.metadata.documentationUrl}>Link
-                                            </ui5.Link>
-                                        </ui5.FormItem>}
-                                </ui5.Form>
-                            </ui5.Panel>
-
-                            <ui5.Panel
-                                headerLevel="H2"
-                                headerText="Plan Details"
-                            >
-                                <ui5.Form>
-                                    <ui5.FormItem label="Plan Name">
-                                        <ui5.Select onChange={onChangeSelect}>
-                                            {
-                                                details?.plans.map((plan, index) =>
-                                                    (
-                                                        <>
-                                                            <ui5.Option
-                                                                data-id={plan.name}
-                                                                key={plan.name}>{plan.name}
-                                                            </ui5.Option>
-                                                        </>
-                                                    ))
-                                            }
-                                        </ui5.Select>
-                                    </ui5.FormItem>
-                                    <ui5.FormItem label="Description">
-                                        <ui5.Text>{plan?.description}</ui5.Text>
-                                    </ui5.FormItem>
-                                </ui5.Form>
-                            </ui5.Panel>
-
-                            <ui5.Panel
-                                accessibleRole="Form"
-                                headerLevel="H2"
-                                headerText="Service Instance Details"
-                            >
-                                <ui5.Form>
-                                    <ui5.FormItem label="Name">
-                                        <ui5.Input style={{width: "100vw"}} required
-                                                   value={generateServiceInstanceName(plan?.name, selectedOffering?.catalogName)}></ui5.Input>
-                                    </ui5.FormItem>
-                                    <ui5.FormItem label="Provisioning Parameters">
-                                        <ui5.TextArea
-                                            style={{width: "100%", height: "100px"}}
-                                            valueState="None"
-                                            title="Provisioning Parameters"
-                                        />
-                                    </ui5.FormItem>
-                                    <ui5.FormItem label="External Name">
-                                        <ui5.Input></ui5.Input>
-                                    </ui5.FormItem>
-                                </ui5.Form>
-                            </ui5.Panel>
-                        </ui5.Dialog>, document.body)}
+                    {portal != null && portal}
                 </>
             );
         });
     };
 
     return <>{renderData()}</>;
-}
-
-function generateServiceInstanceName(plan: string | undefined, service: string | undefined): string {
-    const id = window.crypto.randomUUID().substring(0, 4)
-    return `${service}-${plan}-${id}`;
 }
 
 function splitSecret(secret: string) {
