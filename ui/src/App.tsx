fc9e3761d85fdafab4e6a691b3209976e7d4ba98
ralikio <74771103+ralikio@users.noreply.github.com>
2024-08-05 15:37:40 +0200

    Polishing Create Instance Form

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -7,22 +7,23 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from './components/Layout';
 
 function App() {
-  
+
   const [secret, setSecret] = React.useState("");
 
   return (
     <div id="App" className="App html-wrap flex-container flex-column">
-        <BrowserRouter basename='/' >
-          <Routes>
-            <Route path="/" element={<Layout onSecretChanged={(s: string) => setSecret(s)}/>}>
-              <Route index element={<Navigate to="offerings" replace />} />
-              <Route path="*" element={<Navigate to="offerings" replace />} />
+      <BrowserRouter basename='/' >
+        <Routes>
+          <Route path="/" element={<Layout onSecretChanged={(s: string) => setSecret(s)} />}>
+            <Route index element={<Navigate to="offerings" replace />} />
+            <Route path="*" element={<Navigate to="offerings" replace />} />
 
-              <Route path="/instances" element={<ServiceInstancesView/>}/>
-              <Route path="/offerings" element={<ServiceOfferingsView secret={secret}/>}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+            <Route path="/instances" element={<ServiceInstancesView />} />
+            <Route path="/instances/:id" element={<ServiceInstancesView />} />
+            <Route path="/offerings" element={<ServiceOfferingsView secret={secret} />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
