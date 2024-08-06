0838593f7aec62387f89988c5e447cb555b14c23
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2024-08-06 08:49:24 +0200

    Polishing List Instances View (#780)
    Polishing Create Instance Form

--- ui/src/components/ServiceInstancesView.tsx
+++ ui/src/components/ServiceInstancesView.tsx
@@ -7,12 +7,15 @@ import api from "../shared/api";
 import Ok from "../shared/validator";
 import serviceInstancesData from '../test-data/serivce-instances.json';
 import ServiceInstancesDetailsView from "./ServiceInstancesDetailsView";
+import { useParams } from "react-router-dom";
 
 function ServiceInstancesView() {
   const [serviceInstances, setServiceInstances] = useState<ServiceInstances>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [portal, setPortal] = useState<JSX.Element>();
+
+  let { id } = useParams();
 
   useEffect(() => {
     var useTestData = process.env.REACT_APP_USE_TEST_DATA === "true"
@@ -23,6 +26,12 @@ function ServiceInstancesView() {
         .then((response) => {
           setLoading(false);
           setServiceInstances(response.data);
+          if (id) {
+            const instance = response.data.items.find((instance) => instance.id === id);
+            if (instance) {
+              openPortal(instance)
+            }
+          }
         })
         .catch((error) => {
           setLoading(false);
@@ -34,14 +43,41 @@ function ServiceInstancesView() {
       setServiceInstances(serviceInstancesData)
       setLoading(false);
     }
-  }, []);
+  }, [id]);
 
   if (loading) {
-    return <ui5.Loader progress="100%" />
+    return <ui5.BusyIndicator
+      active
+      delay={1000}
+      size="Medium"
+    />
   }
 
   if (error) {
     return <ui5.IllustratedMessage name="UnableToLoad" />
+  }
+
+  function openPortal(instance: any) {
+    console.log("Row clicked")
+    const instanceView = <ServiceInstancesDetailsView key={instance.id} instance={instance} />
+    const portal = createPortal(instanceView, document.getElementById("App")!!)
+    setPortal(portal)
+  }
+
+  function deleteInstance(id: string): boolean {
+    setLoading(true);
+    
+    axios
+    .delete(api("service-instances") + "/" + id)
+    .then((response) => {
+      setLoading(false);
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error);
+    });
+
+    return true;
   }
 
   const renderData = () => {
@@ -50,18 +86,17 @@ function ServiceInstancesView() {
       return <ui5.IllustratedMessage name="NoEntries" />
     }
     return serviceInstances?.items.map((instance, index) => {
+
+
       return (
         <>
-          <ui5.TableRow 
-          onClick={() => {
-            console.log("Row clicked")
-            const instanceView = <ServiceInstancesDetailsView 
-            key={instance.id} instance={instance} open={true} />
-            const portal = createPortal( instanceView, document.getElementById("App")!!)
-            setPortal(portal)
-          }}
-        >
-            
+          <ui5.TableRow
+            selected={id === instance.id}
+            onClick={() => {
+              openPortal(instance)
+            }}
+          >
+
             <ui5.TableCell>
               <ui5.Label>{instance.name}</ui5.Label>
             </ui5.TableCell>
@@ -69,6 +104,21 @@ function ServiceInstancesView() {
             <ui5.TableCell>
               <ui5.Label>{instance.namespace}</ui5.Label>
             </ui5.TableCell>
+
+            <ui5.TableCell>
+              <ui5.Label>
+                <ui5.Button
+                  design="Default"
+                  icon="delete"
+                  onClick={function _a(e: any) { 
+                    e.stopPropagation();
+                    return deleteInstance(instance.id); 
+                  }}
+                >
+                </ui5.Button>
+              </ui5.Label>
+            </ui5.TableCell>
+
 
           </ui5.TableRow>
         </>
@@ -78,29 +128,30 @@ function ServiceInstancesView() {
 
   return (
     <>
-    {
-      <ui5.Card>
+        <ui5.Card>
 
-        <ui5.Table
-          columns={
-            <>
-              <ui5.TableColumn>
-                <ui5.Label>Service Instance</ui5.Label>
-              </ui5.TableColumn>
+          <ui5.Table
+            columns={
+              <>
+                <ui5.TableColumn>
+                  <ui5.Label>Service Instance</ui5.Label>
+                </ui5.TableColumn>
 
-              <ui5.TableColumn>
-                <ui5.Label>Service Namespace</ui5.Label>
-              </ui5.TableColumn>
-            </>
-          }
-        >
-          {renderData()}
-        </ui5.Table>
-      </ui5.Card>
+                <ui5.TableColumn>
+                  <ui5.Label>Service Namespace</ui5.Label>
+                </ui5.TableColumn>
 
-    }
-    {portal != null && portal}
+                <ui5.TableColumn>
+                  <ui5.Label>Action</ui5.Label>
+                </ui5.TableColumn>
 
+              </>
+            }
+          >
+            {renderData()}
+          </ui5.Table>
+        </ui5.Card>
+      {portal != null && portal}
     </>
   );
 }
