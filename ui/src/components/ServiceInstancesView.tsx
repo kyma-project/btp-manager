8b9ac16d7d9685c9592875deecc563a0e2069122
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2024-08-23 14:21:04 +0200

    Unnecessary Error (#847)
    * PR Update
    
    * Removed Unnecessary Error
    
    * PR Update

--- ui/src/components/ServiceInstancesView.tsx
+++ ui/src/components/ServiceInstancesView.tsx
@@ -114,9 +114,7 @@ function ServiceInstancesView(props: any) {
     if (!Ok(serviceInstances) || !Ok(serviceInstances.items)) {
       return <ui5.IllustratedMessage name="NoEntries" />
     }
-    if (error) {
-      return <ui5.IllustratedMessage name="NoEntries"/>
-    }
+    
     return serviceInstances?.items.map((instance, index) => {
 
 
