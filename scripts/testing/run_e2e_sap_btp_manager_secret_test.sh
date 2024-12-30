6944944beb4d02b564171c9c5fa77c2aa90d1bd9
Marcin Szwed <marcin.szwed@sap.com>
2024-12-30 10:44:38 +0100

    Check if secret with credentials is in management namespace

--- scripts/testing/run_e2e_sap_btp_manager_secret_test.sh
+++ scripts/testing/run_e2e_sap_btp_manager_secret_test.sh
@@ -13,7 +13,7 @@ echo -e "\n--- SAP BTP Manager secret customization test ---"
 
 # Set environment variables
 ## Resources names
-NAMESPACE=kyma-system
+RELEASE_NAMESPACE=kyma-system
 BTP_MANAGER_SECRET_NAME=sap-btp-manager
 SAP_BTP_OPERATOR_SECRET_NAME=sap-btp-service-operator
 SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME=sap-btp-operator-clusterid
@@ -29,27 +29,29 @@ MANAGEMENT_NAMESPACE="management-namespace"
 ENCODED_CLUSTER_ID=$(echo -n ${CLUSTER_ID} | base64)
 ENCODED_MANAGEMENT_NAMESPACE=$(echo -n ${MANAGEMENT_NAMESPACE} | base64)
 
+## Check secret existence in the release namespace
+kubectl get secret -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} && echo "${SAP_BTP_OPERATOR_SECRET_NAME} secret exists in ${RELEASE_NAMESPACE} namespace" || \
+echo "could not get ${SAP_BTP_OPERATOR_SECRET_NAME} secret in ${RELEASE_NAMESPACE} namespace, command return code: $?" && exit 1
+
 ## Save current resourceVersion of the resources to be updated
-SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.metadata.resourceVersion}")
-SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION=$(kubectl get configmap -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.metadata.resourceVersion}")
+SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION=$(kubectl get configmap -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.metadata.resourceVersion}")
 
 ## Save current ID of the resource to be recreated
-SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o jsonpath="{.metadata.uid}")
+SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID=$(kubectl get secret -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o jsonpath="{.metadata.uid}")
 
 ## Variables to track resources changes
-ACTUAL_SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION=${SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION}
 ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION=${SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION}
 ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID=${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID}
 
 ## Conditionals and loops control variables
-SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION_CHANGED=false
+SAP_BTP_OPERATOR_SECRET_CHANGED=false
 SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION_CHANGED=false
 SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID_CHANGED=false
 RESOURCES_CHANGED=false
 
 # Patch the secret with fake values
 echo -e "\n--- Customizing ${BTP_MANAGER_SECRET_NAME} secret with fake values"
-kubectl patch secret -n ${NAMESPACE} ${BTP_MANAGER_SECRET_NAME} -p "{\"data\":{\"clientid\":\"${CLIENT_ID}\",\"clientsecret\":\"${CLIENT_SECRET}\",\"sm_url\":\"${SM_URL}\",\"tokenurl\":\"${TOKEN_URL}\",\"cluster_id\":\"${ENCODED_CLUSTER_ID}\",\"management_namespace\":\"${ENCODED_MANAGEMENT_NAMESPACE}\"}}"
+kubectl patch secret -n ${RELEASE_NAMESPACE} ${BTP_MANAGER_SECRET_NAME} -p "{\"data\":{\"clientid\":\"${CLIENT_ID}\",\"clientsecret\":\"${CLIENT_SECRET}\",\"sm_url\":\"${SM_URL}\",\"tokenurl\":\"${TOKEN_URL}\",\"cluster_id\":\"${ENCODED_CLUSTER_ID}\",\"management_namespace\":\"${ENCODED_MANAGEMENT_NAMESPACE}\"}}"
 
 # Wait until resources are reconciled
 echo -e "\n--- Waiting for SAP BTP service operator secrets and configmap changes"
@@ -60,44 +62,47 @@ do
   if [[ ${SECONDS} -ge ${TIMEOUT} ]]; then
     echo "timed out after ${TIMEOUT}s" && exit 1
   fi
-  if ! ${SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION_CHANGED}; then
-    ACTUAL_SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.metadata.resourceVersion}")
-    if [[ "${ACTUAL_SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION}" != "${SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION}" ]]; then
-      echo "${SAP_BTP_OPERATOR_SECRET_NAME} secret version changed"
-      SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION_CHANGED=true
+  if ! ${SAP_BTP_OPERATOR_SECRET_CHANGED}; then
+    if ! kubectl get secret -n ${MANAGEMENT_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME}; then
+      echo "${SAP_BTP_OPERATOR_SECRET_NAME} secret exists in ${MANAGEMENT_NAMESPACE} namespace"
+      SAP_BTP_OPERATOR_SECRET_CHANGED=true
     fi
   fi
   if ! ${SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION_CHANGED}; then
-    ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION=$(kubectl get configmap -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.metadata.resourceVersion}")
+    ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION=$(kubectl get configmap -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.metadata.resourceVersion}")
     if [[ "${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION}" != "${SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION}" ]]; then
       echo "${SAP_BTP_OPERATOR_CONFIGMAP_NAME} configmap version changed"
       SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION_CHANGED=true
     fi
   fi
   if ! ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID_CHANGED}; then
-    if [[ "$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} 2>&1)" != *"Error from server (NotFound)"* ]]; then
-      ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o jsonpath="{.metadata.uid}")
+    if [[ "$(kubectl get secret -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} 2>&1)" != *"Error from server (NotFound)"* ]]; then
+      ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID=$(kubectl get secret -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o jsonpath="{.metadata.uid}")
       if [[ "${ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID}" != "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID}" ]]; then
         echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret ID changed"
         SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID_CHANGED=true
       fi
     else
-      echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret does not exist in ${NAMESPACE} namespace"
+      echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret does not exist in ${RELEASE_NAMESPACE} namespace"
     fi
   fi
-  if ${SAP_BTP_OPERATOR_SECRET_RESOURCE_VERSION_CHANGED} && ${SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION_CHANGED} && ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID_CHANGED}; then
+  if ${SAP_BTP_OPERATOR_SECRET_CHANGED} && ${SAP_BTP_OPERATOR_CONFIGMAP_RESOURCE_VERSION_CHANGED} && ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_ID_CHANGED}; then
     RESOURCES_CHANGED=true
   fi
   sleep 2
 done
 
+echo -e "\n--- Checking if ${SAP_BTP_OPERATOR_SECRET_NAME} has been removed from ${RELEASE_NAMESPACE} namespace"
+[[ "$(kubectl get secret -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} 2>&1)" = *"Error from server (NotFound)"* ]] && \
+echo "secret has been removed" || echo "secret has not been removed" && exit 1
+
 # Save the current data from secret and configmap
-ACTUAL_SAP_BTP_OPERATOR_SECRET_CLIENT_ID=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.clientid}")
-ACTUAL_SAP_BTP_OPERATOR_SECRET_CLIENT_SECRET=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.clientsecret}")
-ACTUAL_SAP_BTP_OPERATOR_SECRET_SM_URL=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.sm_url}")
-ACTUAL_SAP_BTP_OPERATOR_SECRET_TOKEN_URL=$(kubectl get secret -n ${NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.tokenurl}")
-ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_CLUSTER_ID=$(kubectl get configmap -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.data.CLUSTER_ID}")
-ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_MANAGEMENT_NAMESPACE=$(kubectl get configmap -n ${NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.data.MANAGEMENT_NAMESPACE}")
+ACTUAL_SAP_BTP_OPERATOR_SECRET_CLIENT_ID=$(kubectl get secret -n ${MANAGEMENT_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.clientid}")
+ACTUAL_SAP_BTP_OPERATOR_SECRET_CLIENT_SECRET=$(kubectl get secret -n ${MANAGEMENT_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.clientsecret}")
+ACTUAL_SAP_BTP_OPERATOR_SECRET_SM_URL=$(kubectl get secret -n ${MANAGEMENT_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.sm_url}")
+ACTUAL_SAP_BTP_OPERATOR_SECRET_TOKEN_URL=$(kubectl get secret -n ${MANAGEMENT_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o jsonpath="{.data.tokenurl}")
+ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_CLUSTER_ID=$(kubectl get configmap -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.data.CLUSTER_ID}")
+ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP_MANAGEMENT_NAMESPACE=$(kubectl get configmap -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o jsonpath="{.data.MANAGEMENT_NAMESPACE}")
 
 # Compare secrets and configmap data
 echo -e "\n--- Checking SAP BTP service operator secret and configmap data"
@@ -121,11 +126,11 @@ else
 fi
 
 # Get SAP BTP service operator pod name
-SAP_BTP_OPERATOR_POD_NAME=$(kubectl get pod -n ${NAMESPACE} -l app.kubernetes.io/name=sap-btp-operator -o jsonpath="{.items[*].metadata.name}")
+SAP_BTP_OPERATOR_POD_NAME=$(kubectl get pod -n ${RELEASE_NAMESPACE} -l app.kubernetes.io/name=sap-btp-operator -o jsonpath="{.items[*].metadata.name}")
 
 # Get environment variables from the SAP BTP service operator pod
-ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID=$(kubectl exec -n ${NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv CLUSTER_ID)
-ACTUAL_SAP_BTP_OPERATOR_MANAGEMENT_NAMESPACE=$(kubectl exec -n ${NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv MANAGEMENT_NAMESPACE)
+ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID=$(kubectl exec -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv CLUSTER_ID)
+ACTUAL_SAP_BTP_OPERATOR_MANAGEMENT_NAMESPACE=$(kubectl exec -n ${RELEASE_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv MANAGEMENT_NAMESPACE)
 
 # Check envs in the SAP BTP service operator pod
 echo -e "\n--- Checking ${SAP_BTP_OPERATOR_POD_NAME} pod CLUSTER_ID and MANAGEMENT_NAMESPACE environment variables"
