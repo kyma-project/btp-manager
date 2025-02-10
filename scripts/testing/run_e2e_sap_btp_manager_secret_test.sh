b8f29befa79368c0aab76897c619ce07fef89ee3
Marcin Szwed <marcin.szwed@sap.com>
2025-02-10 08:59:39 +0100

    Add funcs for repeated commands; check corner cases

--- scripts/testing/run_e2e_sap_btp_manager_secret_test.sh
+++ scripts/testing/run_e2e_sap_btp_manager_secret_test.sh
@@ -9,6 +9,90 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
+checkResourcesReconciliation () {
+  local ACTUAL_SAP_BTP_OPERATOR_SECRET=""
+  local ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP=""
+  local ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET=""
+
+  local SAP_BTP_OPERATOR_SECRET_CHANGED=false
+  local SAP_BTP_OPERATOR_CONFIGMAP_CHANGED=false
+  local SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED=false
+  local RESOURCES_CHANGED=false
+
+  # Wait until resources are reconciled
+  echo -e "\n--- Waiting for SAP BTP service operator secrets and configmap changes"
+  echo -e "-- Expected changes:" \
+  "\n- ${SAP_BTP_OPERATOR_SECRET_NAME} and ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secrets exist in $1 namespace" \
+  "\n- ${SAP_BTP_OPERATOR_SECRET_NAME} secret contains updated Service Manager credentials" \
+  "\n- ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret contains updated value of the INITIAL_CLUSTER_ID key" \
+  "\n- ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} ConfigMap in ${KYMA_NAMESPACE} namespace contains the updated values of the following keys: CLUSTER_ID, RELEASE_NAMESPACE, MANAGEMENT_NAMESPACE\n"
+
+  SECONDS=0
+  TIMEOUT=60
+  until $RESOURCES_CHANGED
+  do
+    if [[ ${SECONDS} -ge ${TIMEOUT} ]]; then
+      echo "timed out after ${TIMEOUT}s" && exit 1
+    fi
+    if ! ${SAP_BTP_OPERATOR_SECRET_CHANGED}; then
+      ACTUAL_SAP_BTP_OPERATOR_SECRET=$(kubectl get secret -n $1 ${SAP_BTP_OPERATOR_SECRET_NAME} -o json) && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.clientid)" == "${ENCODED_CLIENT_ID}" ]] && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.clientsecret)" == "${ENCODED_CLIENT_SECRET}" ]] && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.sm_url)" == "${ENCODED_SM_URL}" ]] && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.tokenurl)" == "${ENCODED_TOKEN_URL}" ]] && \
+      echo "${SAP_BTP_OPERATOR_SECRET_NAME} secret exists in $1 namespace and contains updated values" && \
+      SAP_BTP_OPERATOR_SECRET_CHANGED=true
+    fi
+    if ! ${SAP_BTP_OPERATOR_CONFIGMAP_CHANGED}; then
+      ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP=$(kubectl get configmap -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o json) && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.CLUSTER_ID)" == "${CLUSTER_ID}" ]] && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.RELEASE_NAMESPACE)" == "$1" ]] && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.MANAGEMENT_NAMESPACE)" == "$1" ]] && \
+      echo "${SAP_BTP_OPERATOR_CONFIGMAP_NAME} ConfigMap in ${KYMA_NAMESPACE} namespace contains updated values" && \
+      SAP_BTP_OPERATOR_CONFIGMAP_CHANGED=true
+    fi
+    if ! ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED}; then
+      ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET=$(kubectl get secret -n $1 ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o json) && \
+      [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET} | jq -r .data.INITIAL_CLUSTER_ID)" == "${ENCODED_CLUSTER_ID}" ]] && \
+      echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret exists in $1 namespace and contains updated value" && \
+      SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED=true
+    fi
+    if ${SAP_BTP_OPERATOR_SECRET_CHANGED} && ${SAP_BTP_OPERATOR_CONFIGMAP_CHANGED} && ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED}; then
+      RESOURCES_CHANGED=true
+    fi
+    sleep 2
+  done
+}
+
+checkSecretsRemovalFromPreviousNamespace () {
+  echo -e "\n--- Checking if ${SAP_BTP_OPERATOR_SECRET_NAME} has been removed from $1 namespace"
+  ([[ "$(kubectl get secret -n $1 ${SAP_BTP_OPERATOR_SECRET_NAME} 2>&1)" = *"Error from server (NotFound)"* ]] && echo "secret has been removed") || \
+  (echo "secret has not been removed" && exit 1)
+
+  echo -e "\n--- Checking if ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} has been removed from $1 namespace"
+  ([[ "$(kubectl get secret -n $1 ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} 2>&1)" = *"Error from server (NotFound)"* ]] && echo "secret has been removed") || \
+  (echo "secret has not been removed" && exit 1)
+}
+
+checkPodEnvs() {
+  # Get environment variables from the SAP BTP service operator pod
+  ACTUAL_SAP_BTP_OPERATOR_POD_CLUSTER_ID=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv CLUSTER_ID)
+  ACTUAL_SAP_BTP_OPERATOR_POD_RELEASE_NAMESPACE=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv RELEASE_NAMESPACE)
+  ACTUAL_SAP_BTP_OPERATOR_POD_MANAGEMENT_NAMESPACE=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv MANAGEMENT_NAMESPACE)
+
+  # Check envs in the SAP BTP service operator pod
+  echo -e "\n--- Checking ${SAP_BTP_OPERATOR_POD_NAME} pod's CLUSTER_ID, RELEASE_NAMESPACE, MANAGEMENT_NAMESPACE environment variables"
+
+  if [[ "${ACTUAL_SAP_BTP_OPERATOR_POD_CLUSTER_ID}" == "$1" && \
+        "${ACTUAL_SAP_BTP_OPERATOR_POD_RELEASE_NAMESPACE}" == "$2" && \
+        "${ACTUAL_SAP_BTP_OPERATOR_POD_MANAGEMENT_NAMESPACE}" == "$2" ]]; then
+    echo "Environment variables match"
+  else
+    echo "Environment variables do not match"
+    exit 1
+  fi
+}
+
 echo -e "\n--- BTP Manager secret customization test ---\n"
 
 # Set environment variables
@@ -32,6 +116,7 @@ ENCODED_SM_URL=$(echo -n ${SM_URL} | base64)
 ENCODED_TOKEN_URL=$(echo -n ${TOKEN_URL} | base64)
 ENCODED_CLUSTER_ID=$(echo -n ${CLUSTER_ID} | base64)
 ENCODED_CREDENTIALS_NAMESPACE=$(echo -n ${CREDENTIALS_NAMESPACE} | base64)
+ENCODED_KYMA_NAMESPACE=$(echo -n ${KYMA_NAMESPACE} | base64)
 
 ## Check SAP BTP service operator secret existence
 echo -e "\n--- Checking ${SAP_BTP_OPERATOR_SECRET_NAME} existence"
@@ -48,17 +133,6 @@ echo -e "\n--- Checking ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} existence"
 (kubectl get secret -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} && echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace exists") || \
 (echo "could not get ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
 
-## Variables to track resources changes
-ACTUAL_SAP_BTP_OPERATOR_SECRET=""
-ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP=""
-ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET=""
-
-## Conditionals and loops control variables
-SAP_BTP_OPERATOR_SECRET_CHANGED=false
-SAP_BTP_OPERATOR_CONFIGMAP_CHANGED=false
-SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED=false
-RESOURCES_CHANGED=false
-
 # Create credentials namespace if required
 echo -e "\n--- Creating ${CREDENTIALS_NAMESPACE} if required"
 kubectl create namespace ${CREDENTIALS_NAMESPACE} || echo "${CREDENTIALS_NAMESPACE} namespace already exists"
@@ -69,83 +143,73 @@ kubectl patch secret -n ${KYMA_NAMESPACE} ${BTP_MANAGER_SECRET_NAME} -p "{\"data
 (echo "could not patch ${BTP_MANAGER_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
 
 # Wait until resources are reconciled
-echo -e "\n--- Waiting for SAP BTP service operator secrets and configmap changes"
-echo -e "-- Expected changes:" \
-"\n- ${SAP_BTP_OPERATOR_SECRET_NAME} and ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secrets exist in ${CREDENTIALS_NAMESPACE} namespace" \
-"\n- ${SAP_BTP_OPERATOR_SECRET_NAME} secret contains updated Service Manager credentials" \
-"\n- ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret contains updated value of the INITIAL_CLUSTER_ID key" \
-"\n- ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} ConfigMap in ${KYMA_NAMESPACE} namespace contains the updated values of the following keys: CLUSTER_ID, RELEASE_NAMESPACE, MANAGEMENT_NAMESPACE\n"
+checkResourcesReconciliation ${CREDENTIALS_NAMESPACE}
 
-SECONDS=0
-TIMEOUT=60
-until $RESOURCES_CHANGED
-do
-  if [[ ${SECONDS} -ge ${TIMEOUT} ]]; then
-    echo "timed out after ${TIMEOUT}s" && exit 1
-  fi
-  if ! ${SAP_BTP_OPERATOR_SECRET_CHANGED}; then
-    ACTUAL_SAP_BTP_OPERATOR_SECRET=$(kubectl get secret -n ${CREDENTIALS_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} -o json) && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.clientid)" == "${ENCODED_CLIENT_ID}" ]] && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.clientsecret)" == "${ENCODED_CLIENT_SECRET}" ]] && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.sm_url)" == "${ENCODED_SM_URL}" ]] && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_SECRET} | jq -r .data.tokenurl)" == "${ENCODED_TOKEN_URL}" ]] && \
-    echo "${SAP_BTP_OPERATOR_SECRET_NAME} secret exists in ${CREDENTIALS_NAMESPACE} namespace and contains updated values" && \
-    SAP_BTP_OPERATOR_SECRET_CHANGED=true
-  fi
-  if ! ${SAP_BTP_OPERATOR_CONFIGMAP_CHANGED}; then
-    ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP=$(kubectl get configmap -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_CONFIGMAP_NAME} -o json) && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.CLUSTER_ID)" == "${CLUSTER_ID}" ]] && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.RELEASE_NAMESPACE)" == "${CREDENTIALS_NAMESPACE}" ]] && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CONFIGMAP} | jq -r .data.MANAGEMENT_NAMESPACE)" == "${CREDENTIALS_NAMESPACE}" ]] && \
-    echo "${SAP_BTP_OPERATOR_CONFIGMAP_NAME} ConfigMap in ${KYMA_NAMESPACE} namespace contains updated values" && \
-    SAP_BTP_OPERATOR_CONFIGMAP_CHANGED=true
-  fi
-  if ! ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED}; then
-    ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET=$(kubectl get secret -n ${CREDENTIALS_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -o json) && \
-    [[ "$(echo ${ACTUAL_SAP_BTP_OPERATOR_CLUSTER_ID_SECRET} | jq -r .data.INITIAL_CLUSTER_ID)" == "${ENCODED_CLUSTER_ID}" ]] && \
-    echo "${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret exists in ${CREDENTIALS_NAMESPACE} namespace and contains updated value" && \
-    SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED=true
-  fi
-  if ${SAP_BTP_OPERATOR_SECRET_CHANGED} && ${SAP_BTP_OPERATOR_CONFIGMAP_CHANGED} && ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_CHANGED}; then
-    RESOURCES_CHANGED=true
-  fi
-  sleep 2
-done
+# Check if secrets have been removed from the previous namespace
+checkSecretsRemovalFromPreviousNamespace ${KYMA_NAMESPACE}
 
-echo -e "\n--- Checking if ${SAP_BTP_OPERATOR_SECRET_NAME} has been removed from ${KYMA_NAMESPACE} namespace"
-([[ "$(kubectl get secret -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_SECRET_NAME} 2>&1)" = *"Error from server (NotFound)"* ]] && echo "secret has been removed") || \
-(echo "secret has not been removed" && exit 1)
-
-echo -e "\n--- Checking if ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} has been removed from ${KYMA_NAMESPACE} namespace"
-([[ "$(kubectl get secret -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} 2>&1)" = *"Error from server (NotFound)"* ]] && echo "secret has been removed") || \
-(echo "secret has not been removed" && exit 1)
-
-# Get SAP BTP service operator pod name
+# Check SAP BTP service operator pod environment variables
 SAP_BTP_OPERATOR_POD_NAME=$(kubectl get pod -n ${KYMA_NAMESPACE} -l app.kubernetes.io/name=sap-btp-operator -o jsonpath="{.items[*].metadata.name}")
-
-# Get environment variables from the SAP BTP service operator pod
-ACTUAL_SAP_BTP_OPERATOR_POD_CLUSTER_ID=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv CLUSTER_ID)
-ACTUAL_SAP_BTP_OPERATOR_POD_RELEASE_NAMESPACE=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv RELEASE_NAMESPACE)
-ACTUAL_SAP_BTP_OPERATOR_POD_MANAGEMENT_NAMESPACE=$(kubectl exec -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} -c manager -- printenv MANAGEMENT_NAMESPACE)
-
-# Check envs in the SAP BTP service operator pod
-echo -e "\n--- Checking ${SAP_BTP_OPERATOR_POD_NAME} pod's CLUSTER_ID, RELEASE_NAMESPACE, MANAGEMENT_NAMESPACE environment variables"
-
-if [[ "${ACTUAL_SAP_BTP_OPERATOR_POD_CLUSTER_ID}" == "${CLUSTER_ID}" && \
-      "${ACTUAL_SAP_BTP_OPERATOR_POD_RELEASE_NAMESPACE}" == "${CREDENTIALS_NAMESPACE}" && \
-      "${ACTUAL_SAP_BTP_OPERATOR_POD_MANAGEMENT_NAMESPACE}" == "${CREDENTIALS_NAMESPACE}" ]]; then
-  echo "Environment variables match"
-else
-  echo "Environment variables do not match"
-  exit 1
-fi
+checkPodEnvs ${CLUSTER_ID} ${CREDENTIALS_NAMESPACE}
 
 echo -e "\n--- SAP BTP service operator secrets and configmap reconciliation succeeded!"
 
 while [[ $(kubectl get btpoperators/e2e-test-btpoperator -ojson| jq '.status.conditions[] | select(.type=="Ready") |.status+.reason'|xargs)  != "TrueReconcileSucceeded" ]];
-do echo -e "\n---Waiting for BTP Operator to be ready and reconciled"; sleep 5; done
+do echo -e "\n--- Waiting for BTP Operator to be ready and reconciled"; sleep 5; done
 
 echo -e "\n--- BTP Manager secret customization succeeded!"
+
+echo -e "\n--- Checking corner cases..."
+
+# Patch the secret with kyma-system namespace as credentials namespace
+echo -e "\n--- Customizing ${BTP_MANAGER_SECRET_NAME} secret with ${KYMA_NAMESPACE} namespace as credentials namespace"
+kubectl patch secret -n ${KYMA_NAMESPACE} ${BTP_MANAGER_SECRET_NAME} -p "{\"data\":\"credentials_namespace\":\"${ENCODED_KYMA_NAMESPACE}\"}}" || \
+(echo "could not patch ${BTP_MANAGER_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
+
+# Wait until resources are reconciled
+checkResourcesReconciliation ${KYMA_NAMESPACE}
+
+# Check if secrets have been removed from the previous namespace
+checkSecretsRemovalFromPreviousNamespace ${CREDENTIALS_NAMESPACE}
+
+echo -e "\n-- Changing INITIAL_CLUSTER_ID in ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret"
+kubectl patch secret -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} -p "{\"data\":{\"INITIAL_CLUSTER_ID\":\"$(echo -n 'different-cluster-id' | base64)\"}}" || \
+(echo "could not patch ${SAP_BTP_OPERATOR_CLUSTER_ID_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
+
+echo -e "\n-- Deleting ${SAP_BTP_OPERATOR_POD_NAME} pod to enforce CrashLoopBackOff due to invalid cluster ID"
+kubectl delete pod -n ${KYMA_NAMESPACE} ${SAP_BTP_OPERATOR_POD_NAME} || \
+(echo "could not delete ${SAP_BTP_OPERATOR_POD_NAME} pod in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
+
+# Wait until the pod is recreated
+SAP_BTP_OPERATOR_POD_NAME=""
+until [[ -n "${SAP_BTP_OPERATOR_POD_NAME}" ]]; do
+  SAP_BTP_OPERATOR_POD_NAME=$(kubectl get pod -n ${KYMA_NAMESPACE} -l app.kubernetes.io/name=sap-btp-operator -o jsonpath="{.items[*].metadata.name}")
+  sleep 2
+done
+
+# Wait until the pod is in CrashLoopBackOff state
+CRASH_LOOP_BACK_OFF_STATE=""
+until [[ "${CRASH_LOOP_BACK_OFF_STATE}" != "CrashLoopBackOff" ]]; do
+  CRASH_LOOP_BACK_OFF_STATE=$(kubectl get pod ${SAP_BTP_OPERATOR_POD_NAME} -n ${KYMA_NAMESPACE} -o json | jq -r '.status.containerStatuses[] | select(.state.waiting.reason == "CrashLoopBackOff") | .state.waiting.reason')
+  sleep 2
+done
+
+# Trigger the reconciliation by patching the secret with a new client ID
+echo -e "\n--- Customizing ${BTP_MANAGER_SECRET_NAME} secret with new client ID to trigger reconciliation"
+kubectl patch secret -n ${KYMA_NAMESPACE} ${BTP_MANAGER_SECRET_NAME} -p "{\"data\":{\"clientid\":\"$(echo -n 'different-client-id' | base64)\"}" || \
+(echo "could not patch ${BTP_MANAGER_SECRET_NAME} secret in ${KYMA_NAMESPACE} namespace, command return code: $?" && exit 1)
+
+# Wait until resources are reconciled
+checkResourcesReconciliation ${KYMA_NAMESPACE}
+
+# Check SAP BTP service operator pod environment variables
+SAP_BTP_OPERATOR_POD_NAME=$(kubectl get pod -n ${KYMA_NAMESPACE} -l app.kubernetes.io/name=sap-btp-operator -o jsonpath="{.items[*].metadata.name}")
+checkPodEnvs ${CLUSTER_ID} ${CREDENTIALS_NAMESPACE}
+
+while [[ $(kubectl get btpoperators/e2e-test-btpoperator -ojson| jq '.status.conditions[] | select(.type=="Ready") |.status+.reason'|xargs)  != "TrueReconcileSucceeded" ]];
+do echo -e "\n--- Waiting for BTP Operator to be ready and reconciled"; sleep 5; done
+
+echo -e "\n--- Corner cases check succeeded!"
 
 echo -e "\n--- Uninstalling..."
 
