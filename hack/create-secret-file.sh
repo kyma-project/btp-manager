0b770d9f051a0dd0446060f1253edea02ddd370a
Pieszka <jaroslaw.pieszka@sap.com>
2025-04-15 21:05:08 +0200

    script corrected

--- hack/create-secret-file.sh
+++ hack/create-secret-file.sh
@@ -3,8 +3,7 @@ cd "$(dirname "$0")"
 
 # Option tells for which application we are making secret
 OPTION=${1:-'manager'}
-SECRETNAME=${2:-''}
-CREDS=creds.json
+CREDS=${3:-'creds.json'}
 
 if [ ! -f "$CREDS" ]; then
     echo "Required file: $CREDS does not exist."
@@ -12,14 +11,9 @@ if [ ! -f "$CREDS" ]; then
 fi
 
 if [[ $OPTION != 'manager' && $OPTION != 'operator' ]]; then 
-  echo "unsupported option passed $1"
+  echo "unsupported option passed $OPTION"
   exit 1
 fi  
-
-if [[ $OPTION == "operator" && -z $SECRETNAME ]]; then 
-  echo "for option 'operator' secret name parameter is required"
-  exit 1
-fi 
 
 if [ "$OPTION" == "manager" ]; then
 echo 'secret for BTP Manager will be created' 
@@ -33,10 +27,10 @@ metadata:
   labels:
     app.kubernetes.io/managed-by: kcp-kyma-environment-broker
 data:
-  clientid: $(jq --raw-output '.clientid | @base64' creds.json)
-  clientsecret: $(jq --raw-output '.clientsecret | @base64' creds.json)
-  sm_url: $(jq --raw-output '.sm_url | @base64' creds.json)
-  tokenurl: $(jq --raw-output '.url | @base64' creds.json)
+  clientid: $(jq --raw-output '.clientid | @base64' ${CREDS})
+  clientsecret: $(jq --raw-output '.clientsecret | @base64' ${CREDS})
+  sm_url: $(jq --raw-output '.sm_url | @base64' ${CREDS})
+  tokenurl: $(jq --raw-output '.url | @base64' ${CREDS})
   cluster_id: dGVzdF9jbHVzdGVyX2lk
 EOF
 echo ''secret: operator-secret.yaml for BTP Manager created' ' 
@@ -53,15 +47,15 @@ metadata:
   name: $SECRETNAME
   namespace: kyma-system
 data:
-  clientid: $(jq --raw-output '.clientid | @base64' creds.json)
-  clientsecret: $(jq --raw-output '.clientsecret | @base64' creds.json)
-  sm_url: $(jq --raw-output '.sm_url | @base64' creds.json)
-  tokenurl: $(jq --raw-output '.url | @base64' creds.json)
+  clientid: $(jq --raw-output '.clientid | @base64' ${CREDS})
+  clientsecret: $(jq --raw-output '.clientsecret | @base64' ${CREDS})
+  sm_url: $(jq --raw-output '.sm_url | @base64' ${CREDS})
+  tokenurl: $(jq --raw-output '.url | @base64' ${CREDS})
   tokenurlsuffix: L29hdXRoL3Rva2Vu
 EOF
 echo 'secret: btp-access-credentials-secret.yaml with BTP access credentials for SAP BTP Service Operator created'
 exit 0
 fi
 
-echo 'Unsuporrted case'
+echo 'Unsupported case'
 exit 1 
