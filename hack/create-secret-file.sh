668491d7625cc2ba6bc9ab7c96e1980bd2b28482
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2025-04-16 13:02:57 +0200

    Script for secret creation corrected (#1030)
    * script corrected
    
    * parameter list shortened
    
    * fixes
    
    * backward compatibility
    
    * usage
    
    * Missing json

--- hack/create-secret-file.sh
+++ hack/create-secret-file.sh
@@ -1,29 +1,37 @@
 #! /bin/bash
-cd "$(dirname "$0")"
 
-# Option tells for which application we are making secret
-OPTION=${1:-'manager'}
-SECRETNAME=${2:-''}
-CREDS=creds.json
+# Usage examples:
+# ./create-secret-file.sh                        # uses credentials from creds.json file for creations of BTP Manager secret
+# ./create-secret-file.sh <file>                 # uses credentials from specified JSON file for creations of BTP Manager secret
+# ./create-secret-file.sh operator <name>        # uses specified secret name and credentials from creds.json file for creation of SAP BTP Service Operator secret
+# ./create-secret-file.sh operator <name> <file> # uses specified secret name and specified JSON file for creation of SAP BTP Service Operator secret
 
-if [ ! -f "$CREDS" ]; then
-    echo "Required file: $CREDS does not exist."
-    exit 1
-fi
-
-if [[ $OPTION != 'manager' && $OPTION != 'operator' ]]; then 
-  echo "unsupported option passed $1"
-  exit 1
-fi  
-
-if [[ $OPTION == "operator" && -z $SECRETNAME ]]; then 
-  echo "for option 'operator' secret name parameter is required"
-  exit 1
-fi 
-
-if [ "$OPTION" == "manager" ]; then
-echo 'secret for BTP Manager will be created' 
-cat <<EOF > operator-secret.yaml
+if [ "$1" == "operator" ]; then
+  SECRETNAME=$2
+  [[ -z $SECRETNAME ]] && echo "for option 'operator' secret name parameter is required" && exit 1
+  CREDS=${3:-"creds.json"}
+  [[ ! -f "$CREDS" ]] && echo "required file $CREDS not found" && exit 1
+  echo 'secret with BTP access credentials for SAP BTP Service Operator will be created'
+  cat <<EOF >btp-access-credentials-secret.yaml
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  name: ${SECRETNAME}
+  namespace: kyma-system
+data:
+  clientid: $(jq --raw-output '.clientid | @base64' ${CREDS})
+  clientsecret: $(jq --raw-output '.clientsecret | @base64' ${CREDS})
+  sm_url: $(jq --raw-output '.sm_url | @base64' ${CREDS})
+  tokenurl: $(jq --raw-output '.url | @base64' ${CREDS})
+  tokenurlsuffix: L29hdXRoL3Rva2Vu
+EOF
+    echo 'secret: btp-access-credentials-secret.yaml with BTP access credentials for SAP BTP Service Operator created'
+else
+  CREDS=${1:-"creds.json"}
+  [[ ! -f "$CREDS" ]] && echo "required file $CREDS not found" && exit 1
+  echo 'secret for BTP Manager will be created'
+  cat <<EOF >operator-secret.yaml
 apiVersion: v1
 kind: Secret
 type: Opaque
@@ -33,35 +41,11 @@ metadata:
   labels:
     app.kubernetes.io/managed-by: kcp-kyma-environment-broker
 data:
-  clientid: $(jq --raw-output '.clientid | @base64' creds.json)
-  clientsecret: $(jq --raw-output '.clientsecret | @base64' creds.json)
-  sm_url: $(jq --raw-output '.sm_url | @base64' creds.json)
-  tokenurl: $(jq --raw-output '.url | @base64' creds.json)
+  clientid: $(jq --raw-output '.clientid | @base64' ${CREDS})
+  clientsecret: $(jq --raw-output '.clientsecret | @base64' ${CREDS})
+  sm_url: $(jq --raw-output '.sm_url | @base64' ${CREDS})
+  tokenurl: $(jq --raw-output '.url | @base64' ${CREDS})
   cluster_id: dGVzdF9jbHVzdGVyX2lk
 EOF
-echo ''secret: operator-secret.yaml for BTP Manager created' ' 
-exit 0
+  echo 'secret: operator-secret.yaml for BTP Manager created'
 fi
-
-if [ "$OPTION" == "operator" ]; then
-echo 'secret with BTP access credentials for SAP BTP Service Operator will be created' 
-cat <<EOF > btp-access-credentials-secret.yaml
-apiVersion: v1
-kind: Secret
-type: Opaque
-metadata:
-  name: $SECRETNAME
-  namespace: kyma-system
-data:
-  clientid: $(jq --raw-output '.clientid | @base64' creds.json)
-  clientsecret: $(jq --raw-output '.clientsecret | @base64' creds.json)
-  sm_url: $(jq --raw-output '.sm_url | @base64' creds.json)
-  tokenurl: $(jq --raw-output '.url | @base64' creds.json)
-  tokenurlsuffix: L29hdXRoL3Rva2Vu
-EOF
-echo 'secret: btp-access-credentials-secret.yaml with BTP access credentials for SAP BTP Service Operator created'
-exit 0
-fi
-
-echo 'Unsuporrted case'
-exit 1 
