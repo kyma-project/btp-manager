8069c26db7c7b87b75585c287a916919b447cf8f
KsaweryZietara <ksawery.zietara@sap.com>
2026-01-15 12:46:22 +0100

    wip

--- scripts/testing/build-and-push-image.sh
+++ scripts/testing/build-and-push-image.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # Script to build BTP Manager image locally and push it to k3s registry
-# Usage: ./build-and-push-image.sh [VERSION]
+# Usage: ./build-and-push-image.sh [IMAGE_NAME]
 
-VERSION=${1:-"PR-999"}
-REGISTRY="localhost:5000"
-IMAGE_NAME="btp-manager"
+IMAGE_NAME=$1
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -13,11 +11,10 @@ set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
-echo "Building BTP Manager image with version: ${VERSION}"
-docker build -t "${REGISTRY}/${IMAGE_NAME}:${VERSION}" .
+echo "Building ${IMAGE_NAME}"
+docker build -t "${IMAGE_NAME}" .
 
-echo "Pushing ${IMAGE_NAME}:${VERSION} to registry..."
-docker push ${REGISTRY}/${IMAGE_NAME}:${VERSION}
+echo "Pushing ${IMAGE_NAME} to registry..."
+docker push "${IMAGE_NAME}"
 
-echo "${IMAGE_NAME}:${VERSION} built and pushed successfully"
-echo "Images are available in k3s registry at ${REGISTRY}"
+echo "${IMAGE_NAME} built and pushed successfully"
