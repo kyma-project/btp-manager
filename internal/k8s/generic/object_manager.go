0875279585c2d18b3794d515d353d7ec8b5eb4e8
Marcin Szwed <marcin.szwed@sap.com>
2026-02-03 12:28:02 +0100

    Implement delete object

--- internal/k8s/generic/object_manager.go
+++ internal/k8s/generic/object_manager.go
@@ -37,3 +37,10 @@ func (m *ObjectManager[T]) Get(ctx context.Context, key client.ObjectKey, object
 	}
 	return nil
 }
+
+func (m *ObjectManager[T]) Delete(ctx context.Context, object T, opts ...client.DeleteOption) error {
+	if err := m.client.Delete(ctx, object, opts...); err != nil {
+		return fmt.Errorf("while deleting %q: %w", object.GetName(), err)
+	}
+	return nil
+}
