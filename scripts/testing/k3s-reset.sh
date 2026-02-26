c9f35c50ec188dfb0538d9bfa0f598c05bb70949
Pieszka <jaroslaw.pieszka@sap.com>
2026-02-26 13:54:01 +0100

    restart after reset

--- scripts/testing/k3s-reset.sh
+++ scripts/testing/k3s-reset.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-# standard bash error handling
-set -o nounset  # treat unset variables as an error and exit immediately.
-set -o errexit  # exit immediately when a command fails.
-set -E          # needs to be set if we want the ERR trap
-set -o pipefail # prevents errors in a pipeline from being masked
-
 set -x
 
 sudo systemctl stop k3s
 sudo k3s server --cluster-reset
-sudo systemctl start k3s
+sudo systemctl restart k3s
 
 while [[ $(kubectl get nodes -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]];
 do echo "Waiting for cluster nodes to be ready"; sleep 1; done
