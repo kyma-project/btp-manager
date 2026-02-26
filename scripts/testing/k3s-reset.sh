d6c08872c8525902e381407902000b2c3ff9b864
Pieszka <jaroslaw.pieszka@sap.com>
2026-02-26 11:53:08 +0100

    trying to reset cluster

---
+++ scripts/testing/k3s-reset.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+set -x
+
+k3s server --cluster-reset
+
+if [ "${WAIT_OPT}" == "--wait" ]
+then
+  while [[ $(kubectl get nodes -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]];
+  do echo "Waiting for cluster nodes to be ready"; sleep 1; done
+fi
