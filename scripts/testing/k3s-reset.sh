a74065044377bc15b968bf4bc73a800186eeec29
Pieszka <jaroslaw.pieszka@sap.com>
2026-02-26 14:33:27 +0100

    just k3s scripts to uninstall

--- scripts/testing/k3s-reset.sh
+++ scripts/testing/k3s-reset.sh
@@ -2,8 +2,7 @@
 
 set -x
 
-sudo systemctl stop k3s
-sudo k3s server --cluster-reset
+sudo /usr/local/bin/k3s-killall.sh
 sudo /usr/local/bin/k3s-uninstall.sh
 
 echo "Starting K3s cluster (K3s version: ${K3S_VERSION})"
@@ -11,4 +10,3 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} K3S_KUBECONFIG
 mkdir -p ~/.kube
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 chmod 600 ~/.kube/config
-　
