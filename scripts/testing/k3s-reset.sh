00245705cf861ddef90427b5832a4417028e8cbf
Pieszka <jaroslaw.pieszka@sap.com>
2026-02-26 14:18:04 +0100

    another reset

--- scripts/testing/k3s-reset.sh
+++ scripts/testing/k3s-reset.sh
@@ -4,7 +4,11 @@ set -x
 
 sudo systemctl stop k3s
 sudo k3s server --cluster-reset
-sudo systemctl restart k3s
+sudo /usr/local/bin/k3s-uninstall.sh
 
-while [[ $(kubectl get nodes -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]];
-do echo "Waiting for cluster nodes to be ready"; sleep 1; done
+echo "Starting K3s cluster (K3s version: ${K3S_VERSION})"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} K3S_KUBECONFIG_MODE=777 INSTALL_K3S_EXEC="server --disable traefik" sh -
+mkdir -p ~/.kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+chmod 600 ~/.kube/config
+　
