f0d61a7af120e5259e197caa35acbade131aa485
Marcin Szwed <marcin.szwed@sap.com>
2026-02-27 12:10:47 +0100

    Refactor webhook metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,39 +2,32 @@ package metrics
 
 import (
 	"github.com/prometheus/client_golang/prometheus"
-	"sigs.k8s.io/controller-runtime/pkg/metrics"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 const (
 	metricsNamespace = "btpmanager"
 )
 
-type Metrics struct {
-	certsRegenerationsCounter prometheus.Counter
+func buildMetricName(subsystem, name string) string {
+	return prometheus.BuildFQName(metricsNamespace, subsystem, name)
 }
 
-func (m *Metrics) registerMetrics() {
-	//register new custom metrics here, for example:
-	//counter := prometheus.NewCounter(....)
-	//metrics.Registry.MustRegister(counter)
-	certRegenCounter := prometheus.NewCounter(prometheus.CounterOpts{
+type WebhookMetrics struct {
+	certsRegenerationCounter prometheus.Counter
+}
+
+func NewWebhookMetrics(r prometheus.Registerer) *WebhookMetrics {
+	certRegenCounter := promauto.With(r).NewCounter(prometheus.CounterOpts{
 		Name: buildMetricName("", "certs_regenerations_total"),
 		Help: "Total number of certs regenerations",
 	})
-	m.certsRegenerationsCounter = certRegenCounter
-	metrics.Registry.MustRegister(certRegenCounter)
+	m := &WebhookMetrics{
+		certsRegenerationCounter: certRegenCounter,
+	}
+	return m
 }
 
-func (m *Metrics) IncreaseCertsRegenerationsCounter() {
-	m.certsRegenerationsCounter.Inc()
-}
-
-func NewMetrics() *Metrics {
-	metrics := &Metrics{}
-	metrics.registerMetrics()
-	return metrics
-}
-
-func buildMetricName(subsystem, name string) string {
-	return prometheus.BuildFQName(metricsNamespace, subsystem, name)
+func (m *WebhookMetrics) IncrementCertsRegenerationCounter() {
+	m.certsRegenerationCounter.Inc()
 }
