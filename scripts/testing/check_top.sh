7f8dcd03e83f84ad4fec585e8c71f3db967f216e
Pieszka <jaroslaw.pieszka@sap.com>
2026-03-02 11:52:22 +0100

    using file (kind of temp)

--- scripts/testing/check_top.sh
+++ scripts/testing/check_top.sh
@@ -25,12 +25,13 @@ echo -e "\n--- BTP Manager checking kubectl top"
 measure_pod_resources app.kubernetes.io/component=btp-manager.kyma-project.io
 
 echo -e "\n--- BTP Operator checking kubectl top" 
-OPERATOR_TOP=$(measure_pod_resources app.kubernetes.io/name=sap-btp-operator)
-echo ${OPERATOR_TOP}
-OPERATOR_MEM=$(echo ${OPERATOR_TOP} | awk '/sap-btp-operator-controller-manager.*manager/{print $4}' | sed 's/Mi//')
+measure_pod_resources app.kubernetes.io/name=sap-btp-operator| tee top.mem.txt
+
+OPERATOR_MEM=$( awk '/sap-btp-operator-controller-manager.*manager/{print $4}' <top.mem.txt | sed 's/Mi//')
 if [ -n "$OPERATOR_MEM" ] && [ "$OPERATOR_MEM" -le "$MEM_LIMIT" ]; then
      echo "Memory usage of BTP Operator is within the limit: ${OPERATOR_MEM}Mi <= ${MEM_LIMIT}Mi"
 else
      echo "Memory usage of BTP Operator exceeds the limit: ${OPERATOR_MEM}Mi > ${MEM_LIMIT}Mi"
+     echo "See ${REF} for reference."
      exit 1
 fi
