d18872cbb530ee366c6ac6971bad393119b3971f
Pieszka <jaroslaw.pieszka@sap.com>
2026-03-02 11:11:00 +0100

    water marks

--- scripts/testing/check_top.sh
+++ scripts/testing/check_top.sh
@@ -5,6 +5,9 @@ set -o nounset  # treat unset variables as an error and exit immediately.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being masked
 
+REF=$2
+MEM_LIMIT=$(echo $1|sed 's/Mi//')
+
 function measure_pod_resources() {
      TIMEOUT=30
      NEXT_TRY_WAIT=5
@@ -22,4 +25,12 @@ echo -e "\n--- BTP Manager checking kubectl top"
 measure_pod_resources app.kubernetes.io/component=btp-manager.kyma-project.io
 
 echo -e "\n--- BTP Operator checking kubectl top" 
-measure_pod_resources app.kubernetes.io/name=sap-btp-operator
+OPERATOR_TOP=$(measure_pod_resources app.kubernetes.io/name=sap-btp-operator)
+echo ${OPERATOR_TOP}
+OPERATOR_MEM=$(echo ${OPERATOR_TOP} | awk '/sap-btp-operator-controller-manager.*manager/{print $4}' | sed 's/Mi//')
+if [ -n "$OPERATOR_MEM" ] && [ "$OPERATOR_MEM" -le "$MEM_LIMIT" ]; then
+     echo "Memory usage of BTP Operator is within the limit: ${OPERATOR_MEM}Mi <= ${MEM_LIMIT}Mi"
+else
+     echo "Memory usage of BTP Operator exceeds the limit: ${OPERATOR_MEM}Mi > ${MEM_LIMIT}Mi"
+     exit 1
+fi
