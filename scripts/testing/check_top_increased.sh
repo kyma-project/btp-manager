d4e5f8def0d592ae12950fe151e61b5035630b59
Pieszka <jaroslaw.pieszka@sap.com>
2026-03-06 22:00:16 +0100

    initial version of check top2

---
+++ scripts/testing/check_top_increased.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# The script has the following arguments:
+#                       - amount of memory limit for btp-operator in Mi
+# Example:
+#           ./check_top_increased.sh 100 http://link-to-reference.com
+
+
+# standard bash error handling
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+MEM_INCREASE=${1/Mi/}
+
+function measure_pod_resources() {
+     TIMEOUT=30
+     NEXT_TRY_WAIT=5
+     LABEL_SELECTOR=$1
+
+     SECONDS=0
+     while (($SECONDS < $TIMEOUT )); do
+          kubectl top pod -l "$LABEL_SELECTOR" -n kyma-system --containers
+          [ $? == 0 ] && break
+          sleep $NEXT_TRY_WAIT
+     done
+}
+
+echo -e "\n--- BTP Manager checking kubectl top" 
+measure_pod_resources app.kubernetes.io/component=btp-manager.kyma-project.io
+
+echo -e "\n--- BTP Operator checking kubectl top" 
+measure_pod_resources app.kubernetes.io/name=sap-btp-operator| tee top.mem.txt
+
+OPERATOR_MEM=$( awk '/sap-btp-operator-controller-manager.*manager/{print $4}' <top.mem.txt | sed 's/Mi//')
+if [ -n "$OPERATOR_MEM" ] && [ "$OPERATOR_MEM" -le "$MEM_LIMIT" ]; then
+     echo "Memory usage of BTP Operator is within the limit: ${OPERATOR_MEM}Mi <= ${MEM_LIMIT}Mi"
+else
+     echo "Memory usage of BTP Operator exceeds the limit: ${OPERATOR_MEM}Mi > ${MEM_LIMIT}Mi"
+     echo "See ${REF} for reference."
+     exit 1
+fi
