89dfc6cc0743a9435b9e8fd2aa7b124a39f92cce
Pieszka <jaroslaw.pieszka@sap.com>
2026-03-09 10:12:35 +0100

    both paths

--- scripts/testing/check_top_increased.sh
+++ scripts/testing/check_top_increased.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # The script has the following arguments:
-#                       - amount of memory limit for btp-operator in Mi
+#                       - amount of memory we expect to be increased in BTP Operator in Mi after creating large and labeled Secrets (e.g. 1 Mi)
 # Example:
-#           ./check_top_increased.sh 100 http://link-to-reference.com
-
+#           ./check_top_increased.sh 1
 
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
@@ -29,13 +28,16 @@ echo -e "\n--- BTP Manager checking kubectl top"
 measure_pod_resources app.kubernetes.io/component=btp-manager.kyma-project.io
 
 echo -e "\n--- BTP Operator checking kubectl top" 
-measure_pod_resources app.kubernetes.io/name=sap-btp-operator| tee top.mem.txt
+measure_pod_resources app.kubernetes.io/name=sap-btp-operator| tee fin.mem.txt
 
 OPERATOR_MEM=$( awk '/sap-btp-operator-controller-manager.*manager/{print $4}' <top.mem.txt | sed 's/Mi//')
-if [ -n "$OPERATOR_MEM" ] && [ "$OPERATOR_MEM" -le "$MEM_LIMIT" ]; then
-     echo "Memory usage of BTP Operator is within the limit: ${OPERATOR_MEM}Mi <= ${MEM_LIMIT}Mi"
+FIN_OPERATOR_MEM=$( awk '/sap-btp-operator-controller-manager.*manager/{print $4}' <fin.mem.txt | sed 's/Mi//')
+
+TARGET_MEM=$((OPERATOR_MEM + MEM_INCREASE))
+
+if [ -n "$OPERATOR_MEM" ] && [ -n "$FIN_OPERATOR_MEM" ] && [ "$FIN_OPERATOR_MEM" -ge "$TARGET_MEM" ]; then
+     echo "Memory usage of BTP Operator is within the expected limit: ${FIN_OPERATOR_MEM}Mi >= ${TARGET_MEM}Mi"
 else
-     echo "Memory usage of BTP Operator exceeds the limit: ${OPERATOR_MEM}Mi > ${MEM_LIMIT}Mi"
-     echo "See ${REF} for reference."
+     echo "Memory usage of BTP Operator does not exceed the expected limit: ${FIN_OPERATOR_MEM}Mi < ${TARGET_MEM}Mi"
      exit 1
 fi
