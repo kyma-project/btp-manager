b383fc82d1d37b3f70bed522146a2d17072249c5
Pieszka <jaroslaw.pieszka@sap.com>
2026-03-10 09:03:56 +0100

    comment updated

--- scripts/testing/stress-mem-large-labeled-secrets.sh
+++ scripts/testing/stress-mem-large-labeled-secrets.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Stress testing in regard to memory consumption - could cause OOM (but should not).
-# Creates secrets and configmaps in the current context.
+# Creates large labeled secrets in the current context.
 #
 # The script has the following arguments:
 #     - number of large (64KB) Secrets to create (default: 16)
@@ -8,6 +8,7 @@
 #     ./stress-mem-large-labeled-secrets-configmaps.sh 10
 
 M=${1-16}
+
 YAML_DIR=./scripts/testing/yaml
 
 echo -e "\n---Creating ${M} large and labeled Secrets"
@@ -20,7 +21,6 @@ do
   envsubst <${YAML_DIR}/large.labeled.secret.tmpl.yaml | kubectl apply -f - >/dev/null
 done
 
-# Give the cluster some time to react to the new secrets (cache these) and potentially restart pods if OOM occurs. Then check for restarts.
-sleep 10
-
 ./scripts/testing/check_pod_restarts.sh
+
+sleep 10
