a9d1f228a22033b9f02f2cb76e9ec12cf8f58fb9
Marek Michali <marek.michali@sap.com>
2026-07-03 19:08:15 +0200

    Address review comments in component-config automation

--- scripts/create_component_config.sh
+++ scripts/create_component_config.sh
@@ -3,15 +3,15 @@
 # Updates component-config.yaml with the given release tag.
 # Usage: ./scripts/create_component_config.sh <release-tag>
 
-TAG=${1}
-
 # standard bash error handling
 set -o nounset
 set -o errexit
 set -E
 set -o pipefail
 
-CONTROLLER_SOURCE=$(yq '.images[0].source' external-images.yaml)
+TAG=${1}
+
+CONTROLLER_SOURCE=$(yq '.images[] | select(.source | contains("sap-btp-service-operator")) | .source' external-images.yaml)
 
 echo "Updating component-config.yaml for release ${TAG}:"
 
